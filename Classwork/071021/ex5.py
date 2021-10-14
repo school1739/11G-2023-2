446bd72b9444410a08d05ed430711f821030f56a
Фёдор Грачёв <fgrach.2000@gmail.com>
2021-10-14 12:33:45 +0300

    cleanup 2

--- Classwork/071021/ex5.py
+++ Classwork/071021/ex5.py
@@ -1,3 +1,4 @@
+
 bottle_small = int(input("Введите количество бутылок 1 литр и меньше "))
 bottle_big = int(input("Введите количество бутылок, которые больше одного литра "))
 cost_bottle_small = bottle_small * 0.10
