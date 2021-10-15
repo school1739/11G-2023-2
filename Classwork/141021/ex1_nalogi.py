ea1ef3b0553bf5261da88cebb08819b4fe182af7
Владислав <for.programming.77@gmail.com>
2021-10-15 21:29:12 +0300

    сделал первое задание

---
+++ Classwork/141021/ex1_nalogi.py
@@ -0,0 +1,7 @@
+sum = float(input())
+sum_nalog = sum * 0.2
+sum_tea = sum * 0.18
+all = sum + sum_nalog + sum_tea
+print("{0:.2f}".format(sum_nalog))
+print("{0:.2f}".format(sum_tea))
+print("{0:.2f}".format(all))
