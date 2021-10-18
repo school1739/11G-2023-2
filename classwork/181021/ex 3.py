dfb80c56bf243fb38c63af4913537948a8ccd75f
Anikanov Fedor <anikanov.fedor.01@gmail.com>
2021-10-18 12:44:12 +0300

    задание 3

---
+++ classwork/181021/ex 3.py
@@ -0,0 +1,7 @@
+k=str(input())
+if k == 'a' or k == 'e' or k == 'i' or k == 'o' or k == 'u':
+    print('k-гласная буква')
+elif k == 'y':
+    print('k-согласная и гласная буква')
+else:
+    print('k-согласная буква')
