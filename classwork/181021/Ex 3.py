84f8f81a9845f10cf9147278c62cea4359a1b32f
Залевская Вероника <zalev135@gmail.com>
2021-10-18 14:25:52 +0300

    сделал 3 номер

---
+++ classwork/181021/Ex 3.py
@@ -0,0 +1,7 @@
+let=str(input('буква латинского алфавита'))
+if let=="a" or let=="e" or let=="u" or let=="i"or let=="o":
+    print('гласная')
+elif let=="y":
+    print("и согласная и глассная")
+else:
+    print("эта буква согласная")
