880c762546ee8ddf9aae5a7885fc455566b64c18
Ryzhov <sasaryzov153@gmail.com>
2021-10-18 14:39:50 +0300

    Исправила 4 задание

--- Classwork/181021/Ex4.py
+++ Classwork/181021/Ex4.py
@@ -1,17 +1,17 @@
 a = int(input())
-if a == '3':
+if a == 3:
     print('Треугольник')
-elif a == '4':
+elif a == 4:
     print('Квадрат')
-elif a == '5':
+elif a == 5:
     print('Пятиугольник')
-elif a == '6':
+elif a == 6:
     print('Шестиугольник')
-elif a == '7':
+elif a == 7:
     print('Семиугольник')
-elif a == '8':
+elif a == 8:
     print('Восьмиугольник')
-elif a == '9':
+elif a == 9:
     print('Девятиугольник')
 elif a == 10:
     print('Десятиугольник')
