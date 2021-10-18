7e4cfd55e6f3a83e438bbfa13c2043b15c08f28a
Anikanov Fedor <anikanov.fedor.01@gmail.com>
2021-10-18 14:40:44 +0300

    задание 5

---
+++ classwork/181021/ex 5.py
@@ -0,0 +1,25 @@
+n=str(input())
+if n == 'январь':
+    print('31')
+elif n == 'февраль':
+    print('28 или 29')
+elif n == 'март':
+    print('31')
+elif n == 'апрель':
+    print('30')
+elif n == 'май':
+    print('31')
+elif n == 'июнь':
+    print('30')
+elif n == 'июль':
+    print('31')
+elif n == 'август':
+    print('31')
+elif n == 'сентябрь':
+    print('30')
+elif n == 'октябрь':
+    print('31')
+elif n == 'ноябрь':
+    print('30')
+elif n == 'декабрь':
+    print('31')
