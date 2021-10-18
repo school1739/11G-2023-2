27ca19d03d88e742bb779f57332ed0335de209d7
Bormotoon <bormotoon@gmail.com>
2021-10-18 14:24:12 +0300

    Example with comments.

--- Lessons/October/181021/Example with comments.py
+++ Lessons/October/181021/Example with comments.py
@@ -3,11 +3,11 @@ pointX = int(input("Введите X: "))
 pointY = int(input("Введите Y: "))
 
 # Начало условного перехода
-if pointX > 0 and pointY > 0: # Проверка на 1 четверть
-    print("1 четверть") # Вывод ответа, если удовлетворяет условию
+if pointX > 0 and pointY > 0:  # Проверка на 1 четверть
+    print("1 четверть")  # Вывод ответа, если удовлетворяет условию
 elif pointX < 0 and pointY > 0:
     print("2 четверть")
 elif pointX < 0 and pointY < 0:
     print("3 четверть")
-else: # ВСЕ ОСТАЛЬНЫЕ СЛУЧАИ
+else:  # ВСЕ ОСТАЛЬНЫЕ СЛУЧАИ
     print("4 четверть")
