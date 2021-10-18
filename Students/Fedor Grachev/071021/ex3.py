3c6d0d174a49b70961113c4bcb04730f2b5e96f0
Bormotoon <bormotoon@gmail.com>
2021-10-18 09:23:41 +0300

    Module 1 cleaned up. Students and Red Riding Hood are killed.

--- Students/Fedor Grachev/071021/ex3.py
+++
@@ -1,5 +0,0 @@
-length = int(input("Введите длину"))
-high = int(input("Введите высоту"))
-wise = int(input("Введите ширину"))
-
-print("Площадь комнаты:" + str(length * high * wise))
--- Students/Fedor Grachev/Fedor Grachev.py
+++
@@ -1,2 +0,0 @@
-print("Hello, world!")
-print("Меня зовут Фёдор Грачёв.")
