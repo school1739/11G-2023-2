d7c4fadd9abcede7fe4fccc0cc2c3528003ece42
HVGrach <fgrach.2000@gmail.com>
2021-10-18 21:44:21 +0300

    сделал 4 задание

---
+++ Classwork/181021/ex4.py
@@ -0,0 +1,9 @@
+kolvo = int(input())
+if kolvo < 3 or kolvo > 10:
+    print("Вы ввели некорректное количество сторон")
+if kolvo == 3:
+    print("Треугольник")
+elif kolvo == 4:
+    print("Квадрат")
+else:
+    print("многоугольник")
