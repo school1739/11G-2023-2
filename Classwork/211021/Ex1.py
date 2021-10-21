ae37ede7fd4dd37854cbb4d79bec0ce893b30dc0
Lisa Kulakova <kulakova.elizaveta0509@gmail.com>
2021-10-21 11:55:20 +0300

    Не доделала 1

---
+++ Classwork/211021/Ex1.py
@@ -0,0 +1,13 @@
+day = int(input())
+month = int(input())
+year = int(input())
+if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+    print('Высокосный')
+else:
+    print('Обычный')
+if (month == 'Апрель' or month == 'Июнь' or month =='Сентябрь' or month =='Ноябрь') and day == 30:
+     new_month = month + 1
+elif (month == 'Январь' or month =='Март' or month == 'Май' or month =='Июль' or month == 'Август' or month == 'Октябрь' or month == 'Декабрь') and day == 31:
+    print('31')
+else:
+    print('28 и 29')
