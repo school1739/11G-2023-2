ac6cd3b26cbd4b14a91defc61590f6f00c7d81dd
HaHHHaH <zinchenko05@inbox.ru>
2021-10-24 15:18:51 +0300

    Упражнение 5. Арифметика

---
+++ Homework/141021/Ex5.py
@@ -0,0 +1,12 @@
+import math
+a = int(input('Введите число a'))
+b = int(input('Введите число b'))
+print(a + b)
+print(a - b)
+print(a * b)
+print(float(a / b)) # Здесь нужен float или нет?
+print(a % b)
+print(math.log10(a))
+print(math.pow(a, b))
+
+
