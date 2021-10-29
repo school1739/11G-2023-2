30d9fbbd37099903fa46c2699633b835248b7903
Владислав <for.programming.77@gmail.com>
2021-10-29 21:24:52 +0300

    сделал третье задание(ОпСоС)

---
+++ Classwork/211021/ex_3(ОпСоС).py
@@ -0,0 +1,22 @@
+user_minute = int(input("Введите количество израсходованных минут: "))
+user_sms = int(input("Введите количество израсходованных sms: "))
+cost_min = 0
+cost_sms = 0
+
+if user_minute > 50:
+    cost_min = user_minute - 50
+    cost_min = cost_min * 0.25
+
+if user_sms > 50:
+    cost_sms = user_sms - 50
+    cost_sms = cost_sms * 0.15
+
+if user_sms > 50 and user_minute > 50:
+    print("Сумма доп минут и смс: " + str(cost_min + cost_sms))
+
+all_sum = 15 + 0.44 + cost_min + user_sms
+
+print("базовая сумма тарификации: {0:.2f}".format((15 + 0.44)*1.05))
+print("Проц колл центрам: {0:.2f}".format(0.44))
+print("Налог: {0:.2f}".format(all_sum * 1.05 - all_sum))
+print("Вся сумма: {0:.2f}".format(all_sum * 1.05))
