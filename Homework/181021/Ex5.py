4d56a703adca1b1a52476cfdabc91eb24d011713
HaHHHaH <zinchenko05@inbox.ru>
2021-11-03 00:37:18 +0300

    Exercise 5. Сколько дней в месяце?

---
+++ Homework/181021/Ex5.py
@@ -0,0 +1,25 @@
+a = input() # Нужно ли использовать str?
+if a == "январь":
+    print("31")
+elif a == "февраль":
+    print("28 или 29")
+elif a == "март":
+    print("31")
+elif a == "апрель":
+    print("30")
+elif a == "май":
+    print("31")
+elif a == "июнь":
+    print("30")
+elif a == "июль" :
+    print("31")
+elif a == "август":
+    print("31")
+elif a == "сентябрь":
+    print("30")
+elif a == "октябрь":
+    print("31")
+elif a == "ноябрь":
+    print("30")
+elif a == "декабрь":
+    print("31")
