d2c07e9d9bb0cbf7e16cc1c97e0eea0ad3be21fd
Anikanov Fedor <anikanov.fedor01@gmail.com>
2021-11-07 22:00:42 +0300

    номер 4 (недоделанный)

---
+++ classwork/211021/ex 4.py
@@ -0,0 +1,11 @@
+import datatime
+day = int(input())
+month = int(input())
+year = int(input())
+x= datatime.data(year,month,day)
+if (day>=21 and day <= 31 and month == 3) or (day>=1 and day <= 19 and month == 4):
+    print("Овен")
+if (day >= 21 and day <= 31 and month == 3) or (day >= 1 and day <= 19 and month == 4):
+        print("Овен")
+if (day >= 21 and day <= 31 and month == 3) or (day >= 1 and day <= 19 and month == 4):
+        print("Овен")
