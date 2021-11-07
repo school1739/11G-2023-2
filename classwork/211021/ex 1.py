631bbb628d885f1b97ab31b53ded567abcf0d040
Anikanov Fedor <anikanov.fedor01@gmail.com>
2021-11-07 21:58:23 +0300

    номер 1

--- classwork/211021/ex 1.py
+++ classwork/211021/ex 1.py
@@ -2,5 +2,5 @@ import datetime
 year=int(input())
 month=int(input())
 day=int(input())
-tomorrow=datetime.date(year,month,day)+datetime.timedelta(weeks=1)
+tomorrow=datetime.date(year,month,day)+datetime.timedelta(days=1)
 print(tomorrow)
