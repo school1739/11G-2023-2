7ae1bfb873552b2b46f45c64a71c133badab20ad
Anikanov Fedor <anikanov.fedor01@gmail.com>
2021-11-07 15:32:47 +0300

    номер 1

--- classwork/211021/ex 1.py
+++ classwork/211021/ex 1.py
@@ -1,33 +1,6 @@
-year = int(input())
-day = int(input())
-month = int(input())
-m = year + 1
-if day == '31' and month == 'декабрь'
-    print(m)
-if month == 'январь':
-    print('31')
-elif month == 'февраль':
-    print('28 или 29')
-elif month == 'март':
-    print('31')
-elif month == 'апрель':
-    print('30')
-elif month == 'май':
-    print('31')
-elif month == 'июнь':
-    print('30')
-elif month == 'июль':
-    print('31')
-elif month == 'август':
-    print('31')
-elif month == 'сентябрь':
-    print('30')
-elif month == 'октябрь':
-    print('31')
-elif month == 'ноябрь':
-    print('30')
-elif month == 'декабрь':
-    print('31')
-
-
-
+import datetime
+year=int(input())
+month=int(input())
+day=int(input())
+tomorrow=datetime.date(year,month,day)+datetime.timedelta(weeks=1)
+print(tomorrow)
