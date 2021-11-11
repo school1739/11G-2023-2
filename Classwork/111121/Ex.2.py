3fe198a7c4893e66aebbb916b0c577eb42dfaf8f
Lisa <kulakova.elizaveta0509@gmail.com>
2021-11-11 12:45:11 +0300

    Ex.2 (недоделано)

---
+++ Classwork/111121/Ex.2.py
@@ -0,0 +1,18 @@
+### Задание 2. Никаких центов!
+##
+# Запрашиваем у пользователя цены
+#
+cost=int(input())
+S=0
+while cost!= "" or cost!= " ":
+    S=S+cost
+    if S%5<=2:
+
+        print(S)
+
+        break
+    else:
+
+        print(S)
+
+        break
