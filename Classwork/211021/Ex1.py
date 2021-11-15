bb59c1e4d554ca792d514c4294bf19ef35a5eebb
Bormotoon <bormotoon@gmail.com>
2021-11-15 10:23:54 +0300

    21.10.2021 evaluation.

--- Classwork/211021/Ex1.py
+++ Classwork/211021/Ex1.py
@@ -6,8 +6,10 @@ if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
 else:
     print('Обычный')
 if (month == 'Апрель' or month == 'Июнь' or month =='Сентябрь' or month =='Ноябрь') and day == 30:
-     new_month = month + 1
+    new_month = month + 1 # Опечатка в отступе
 elif (month == 'Январь' or month =='Март' or month == 'Май' or month =='Июль' or month == 'Август' or month == 'Октябрь' or month == 'Декабрь') and day == 31:
     print('31')
 else:
     print('28 и 29')
+
+# Evaluation: OK
