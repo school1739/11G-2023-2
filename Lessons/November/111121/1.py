eb5f9ac0ee8427493921b2064f0513266ecc4fef
Ryzhov <sasaryzov153@gmail.com>
2021-11-22 01:11:17 +0300

    сделал №2

--- Lessons/November/111121/1.py
+++ Lessons/November/111121/1.py
@@ -2,9 +2,9 @@ a=int(input())
 b=0
 c=0
 if a==0:
-    print("Error")
+    print("Error")#вывод ошибки при а=0
 else:
-    while a!=0:
+    while a!=0:#b-сумма вводимых чисел,с-кол-во вводимых чисел
         b=b+a
         a = int(input())
         c=c+1
