82bfe14f57690d8a96a9a80a33f0cd30cf825ff9
Ryzhov <sasaryzov153@gmail.com>
2021-11-22 13:36:46 +0300

    сделал №1

---
+++ Lessons/November/151121/1.py
@@ -0,0 +1,5 @@
+a=int(input())+1#т.к range от 0 до а-1, прибавляем 1
+for i in range(a):
+    print("")
+    for j in range(a):#цикл в цикле, с перемножением 2 переменных
+        print(i*j,end="  ")
