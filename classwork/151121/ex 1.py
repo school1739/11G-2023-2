e8e308856fdd4eaf26dd3f0375b9a858be659a87
Anikanov Fedor <anikanov.fedor01@gmail.com>
2021-11-22 22:38:20 +0300

    задание 1

---
+++ classwork/151121/ex 1.py
@@ -0,0 +1,12 @@
+max=int(input())
+row=0
+column=0
+print('', end='\t')
+for column in range(max+1):
+    print(column, end="\t")
+print()
+for row in range(max+1):
+    print(row,end='\t')
+    for column in range(max+1):
+        print(row*column, end='\t')
+    print('')
