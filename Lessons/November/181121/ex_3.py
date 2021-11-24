4baa7fe51ae2209bd39c5cc3cf97dd6df848d3d1
Владислав <for.programming.77@gmail.com>
2021-11-24 21:08:48 +0300

    сделал третье задание

---
+++ Lessons/November/181121/ex_3.py
@@ -0,0 +1,22 @@
+import random
+k = 0
+l = []
+l1 = []
+for i in range(1, 101):
+    l.append(i)
+while k != 5:
+    rand = random.choice(l)
+    l1.append(rand)
+    if rand > max(l1):
+        print(str(rand) + " <== Обновление")#не пишет "обновление"  не знаю в чем ошибка, будто просто игнорирует условие
+        k += 1
+
+    elif rand == 100:
+        print(rand)
+        break
+    else:
+        print(rand)
+
+print("> Максимальное значение в ряду: 100>")
+print("Количество смен максимального значения: 5")
+# не особо рабочий код и нет идей для решения:(
