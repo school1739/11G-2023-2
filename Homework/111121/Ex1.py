243c2e1c1a46fb49690e72bbc8c8fa0c8cb6f75d
HaHHHaH <zinchenko05@inbox.ru>
2021-11-27 00:21:31 +0300

    # TODO Задание 1. Среднее значение

---
+++ Homework/111121/Ex1.py
@@ -0,0 +1,14 @@
+a = int(input())  # input a number
+
+if a == 0:  # if the first number is 0 you will receive a error
+    print('Error')
+
+i = 0  # cycle counter
+b = a
+
+while a != 0:
+    a = int(input())  # input a number
+    b = b + a
+    i += 1
+    if a == 0:
+        print(b / i)  # you get the average sum of numbers which you inputted earlier
