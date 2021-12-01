c3ecbc9ef414c851a250d2365256cb3d81eb5b3e
Владислав <for.programming.77@gmail.com>
2021-12-01 19:07:14 +0300

    сделал второе задание(круг с точками)

---
+++ Lessons/November/291121/2 - Circle.py
@@ -0,0 +1,20 @@
+import math
+radius = 42
+S = round(math.pi * radius**2, 4)
+print(S)
+
+
+point_1 = (23, 34)
+
+t_1 = math.sqrt(23 ** 2 + 34 ** 2)
+if t_1 < radius:
+    print(True)
+else:
+    print(False)
+
+point_2 = (30, 30)
+t_2 = math.sqrt(30 ** 2 + 30 ** 2)
+if t_2 < radius:
+    print(True)
+else:
+    print(False)
