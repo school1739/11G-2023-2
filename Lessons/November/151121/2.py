c1227d31d07754ac1e7893bf81ccc3848285f521
Ryzhov <sasaryzov153@gmail.com>
2021-12-01 18:06:00 +0300

    Рыжов, ex2 15.11.21

---
+++ Lessons/November/151121/2.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=int(input())
+c=0
+d=0
+if a>b:#уменьшение памяти путем перебора до наименьшего числа
+    for i in range(b):
+        c=c+1#счетчик
+        if b%c==0 and a%c==0:#проверка
+            d=c#при прохождении проверки d=max c, т.к с увеличивается
+else:
+    for i in range(a):#тоже самое
+        c=c+1
+        if a%c==0 and b%c==0:
+            d=c
+print(d)
