523a24a115a083b98271e006031cba73d1555c0f
Тимур Покровский <timokrat@yandex.ru>
2021-12-06 12:18:08 +0300

    Сделал Ex.4 на 111121

---
+++ Homework/November/111121/Ex.3.py
@@ -0,0 +1,28 @@
+import math
+
+
+# функция для расчета расстояния
+def distance(p1, p2):
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+
+# первая координата
+first_point = (float(input("x =")), float(input("y =")))
+last_point = first_point
+perimeter = 0
+
+# сюда пусть вставляет числа чел
+while True:
+    x = input("x=")
+
+    if x == '':
+        perimeter += distance(last_point, first_point)
+        break
+
+    y = input("y=")
+
+    new_point = (float(x), float(y))
+    perimeter += distance(new_point, last_point)
+    last_point = new_point
+
+print("Perimeter = {0:.2f}".format(perimeter))
