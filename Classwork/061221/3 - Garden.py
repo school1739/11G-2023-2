6342e8422786b373a201108ddefcefdff7952964
ladyk <lady.khodina@yandex.ru>
2021-12-08 18:52:07 +0300

    Ex.3

--- Classwork/061221/3 - Garden.py
+++ Classwork/061221/3 - Garden.py
@@ -1,28 +1,32 @@
+### Задание №3
+##
 # в саду сорвали цветы
+#
 garden = ('ромашка', 'роза', 'одуванчик', 'ромашка', 'гладиолус', 'подсолнух', 'роза')
-
 # на лугу сорвали цветы
+#
 meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер', 'мак', 'одуванчик', 'ромашка')
-
-# создайте множество цветов, произрастающих в саду и на лугу
-# garden_set =
-# meadow_set =
-# TODO здесь ваш код
+# создадим множество цветов, произрастающих в саду и на лугу
+#
 garden_set = set(garden)
 meadow_set = set(meadow)
-# выведите на консоль все виды цветов
-# TODO здесь ваш код
+# выведем на консоль все виды цветов
+#
 union_set = garden_set.union(meadow_set)
 for flower in union_set:
     print(flower)
-# выведите на консоль те, которые растут и там и там
-# TODO здесь ваш код
-itamitam_set=garden_set & meadow_set
+# выведем на консоль те, которые растут и там и там
+#
+itamitam_set = garden_set & meadow_set
 for flower in itamitam_set:
     print(flower)
-# выведите на консоль те, которые растут в саду, но не растут на лугу
-# TODO здесь ваш код
-
-# выведите на консоль те, которые растут на лугу, но не растут в саду
-# TODO здесь ваш код
-
+# выведем на консоль те, которые растут в саду, но не растут на лугу
+#
+only_in_garden_set = garden_set - meadow_set
+for flower in only_in_garden_set:
+    print(flower)
+# выведем на консоль те, которые растут на лугу, но не растут в саду
+#
+only_in_meadow_set = meadow_set - garden_set
+for flower in only_in_meadow_set:
+    print(flower)
