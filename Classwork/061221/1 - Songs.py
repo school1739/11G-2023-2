8d055d44a94166ed70ecdbe1d33ee331e5a185a8
ladyk <lady.khodina@yandex.ru>
2021-12-08 18:25:10 +0300

    Ex.1

--- Classwork/061221/1 - Songs.py
+++ Classwork/061221/1 - Songs.py
@@ -1,5 +1,7 @@
+### Задание №1
+##
 # Есть список песен группы Depeche Mode со временем звучания с точностью до долей минут
-
+#
 violator_songs = [
     ['World in My Eyes', 4.86],
     ['Sweetest Perfection', 4.43],
@@ -12,14 +14,17 @@ violator_songs = [
     ['Clean', 5.83],
 ]
 
-# Распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
+# Распечатаем общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
 #   Три песни звучат ХХХ.XX минут
-# Точность указывается в функции round(a, b)
-# где a, это число которое надо округлить, а b количество знаков после запятой
-# более подробно про функцию round смотрите в документации https://docs.python.org/3/search.html?q=round
 
-# TODO здесь ваш код
-
+Halo = violator_songs[3]
+dlitelnost_Halo = Halo[1]
+E_t_S = violator_songs[5]
+dlitelnost_E_t_S = E_t_S[1]
+Clean = violator_songs[8]
+dlitelnost_Clean = Clean[1]
+all_dlitelnost = round(dlitelnost_Clean + dlitelnost_Halo + dlitelnost_E_t_S, 2)
+print("Три песни звучат", all_dlitelnost, "минут")
 # Есть словарь песен группы Yellow со временем звучания с точностью до долей минут
 pocket_universe_songs = {
     'Solar Driftwood': 1.85,
@@ -35,10 +40,11 @@ pocket_universe_songs = {
     'Beyond Mirrors': 5.82,
 }
 
-# Распечатайте общее время звучания трех песен: 'On Track', 'To the Sea' и 'Beyond Mirrors'
+# Распечатаем общее время звучания трех песен: 'On Track', 'To the Sea' и 'Beyond Mirrors'
 #   А другие три песни звучат приблизительно ХХХ минут
 
-# TODO здесь ваш код
-
-# Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
-# Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
+O_T = pocket_universe_songs['On Track']
+T_t_S = pocket_universe_songs['To the Sea']
+B_M = pocket_universe_songs['Beyond Mirrors']
+dlitelnost = round(O_T + T_t_S + B_M)
+print("А другие три песни звучат приблизительно", dlitelnost, "минут")
