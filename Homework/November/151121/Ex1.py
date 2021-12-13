f93ab3c2ddf74eda85a527b86e04b3ca4da25566
Армине Григорян <azaz.ag05@gmail.com>
2021-12-13 14:29:02 +0300

    Ex1 1511

---
+++ Homework/November/151121/Ex1.py
@@ -0,0 +1,9 @@
+print('', end='\t')
+for i in range(0, 13): #Строчка т.у.
+    print(i, end='\t')
+print()
+for i in range(0, 13): #Таблица умножения
+    for k in range(0, 13):
+        print(k*i, end='\t')
+    print()
+
