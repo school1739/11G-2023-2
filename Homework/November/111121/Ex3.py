aecca0589a9619a1d31e9910f365b6d423210d09
Армине Григорян <azaz.ag05@gmail.com>
2021-12-21 18:34:43 +0300

    Ex3 1111 Исправленный

--- Homework/November/111121/Ex3.py
+++ Homework/November/111121/Ex3.py
@@ -1,25 +1,24 @@
-
-
 import math
-x1 = input('Введите первую координату X: ')
-y1 = input('Введите первую координату Y: ')
-x2 = input('Введите следующую координату X: ')
-y2 = input('Введите следующую координату Y: ')
+
+x1 = input("Введите координату первой точки по Х: ")
+y1 = input("Введите координату первой точки по У: ")
+x2 = input("Введите координату второй точки по Х: ")
+y2 = input("Введите координату второй точки по У: ")
 P = 0
-fpx=x1
-fpy=y1
-while x1!='' or y1!='' or x2!='' or y2!='':
-    if x2=='' or y2=='':
-        x2 = int(fpx)
-        y2 = int(fpy)
+first_point_x = x1
+first_point_y = y1
+while x1 != "" or y2 != "" or x2 != "" or y2 != "":
+    if x2 == "" or y2 == "":  # Проверка на пустую строку
+        x2 = int(first_point_x)
+        y2 = int(first_point_y)
         break
-    length = abs(math.sqrt((int(x2) - int(x1))**2 + (int(y2)) - int(y1))**2)
-    p = p + length # периметр
+    length = abs(math.sqrt((int(x2) - int(x1)) ** 2 + (int(y2) - int(y1)) ** 2))  # Вычисляем длину стороны фигуры
+    P = P + length  # Вычисляем периметр на данной стадии
     x1 = int(x2)
     y1 = int(y2)
-    # Следующие координаты
-    x2 = input()
-    y2 = input()
-length = math.sqrt((int(fpx) - int(x1)) ** 2 + (int(fpy) -int(y1)) **2)
-p = p + length # итоговый периметр
-print("%.2f" %p)
+    x2 = input("Введите координату следующей точки по Х: ")
+    y2 = input("Введите координату следующей точки по У: ")
+length = math.sqrt(
+    (int(first_point_x) - int(x1)) ** 2 + (int(first_point_y) - int(y1)) ** 2)  # Вычисляем длину последней стороны
+P = P + length  # Вычисляем итоговый периметр
+print("%.2f" % P)  # Выводим результат
