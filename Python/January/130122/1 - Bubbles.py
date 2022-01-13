6e9c2e09ae20ba7b441455b5499a8f35e90de586
LenovoUser <gavrikovvalentin9@yandex.ru>
2022-01-13 12:46:11 +0300

    ex.1 .....

---
+++ Python/January/130122/1 - Bubbles.py
@@ -0,0 +1,27 @@
+import simple_draw as sd
+def puzyirik(start, shag):
+    center_position = sd.get_point(56, 76)
+    COLOR_GREEN = (0, 255, 0)
+    sd.circle(center_position, radius=50, color=COLOR_GREEN, width=1)
+    sd.circle(center_position, radius=45, color=COLOR_GREEN, width=1)
+    sd.circle(center_position, radius=40, color=COLOR_GREEN, width=1)
+# Нарисовать пузырек - три вложенных окружности с шагом 5 пикселей
+center_position=sd.get_point(56,76 )
+COLOR_GREEN = (0, 255, 0)
+sd.circle(center_position, radius=50, color=COLOR_GREEN, width=1)
+sd.circle(center_position, radius=45, color=COLOR_GREEN, width=1)
+sd.circle(center_position, radius=40, color=COLOR_GREEN, width=1)
+sd.pause()
+# Написать функцию рисования пузырька, принимающую 2 (или более) параметра: точка рисовании и шаг
+
+start=sd.get_point(56,76 )
+COLOR_GREEN = (0, 255, 0)
+sd.circle(center_position, radius=50, color=COLOR_GREEN, width=1)
+sd.circle(center_position, radius=45, color=COLOR_GREEN, width=1)
+sd.circle(center_position, radius=40, color=COLOR_GREEN, width=1)
+
+# Нарисовать 10 пузырьков в ряд
+
+# Нарисовать три ряда по 10 пузырьков
+
+# Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
