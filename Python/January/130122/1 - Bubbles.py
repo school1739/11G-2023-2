517c7349da5c8e551ced1cb59441a3d41257ebea
ladyk <lady.khodina@yandex.ru>
2022-01-19 19:21:23 +0300

    ex.5

--- Python/January/130122/1 - Bubbles.py
+++ Python/January/130122/1 - Bubbles.py
@@ -11,27 +11,30 @@ def puzyirik(point, step, colour):
 
 
 # Нарисовать пузырек - три вложенных окружности с шагом 5 пикселей
-point = sd.get_point(300, 300)
-COLOR_GREEN = (0, 255, 0)
+point = sd.get_point(300, 550)
+COLOR_WHITE = (255, 255, 255)
 radius = 50
 for i in range(3):
     radius -= 5
-    sd.circle(center_position=point, radius=radius, color=COLOR_GREEN, width=1)
+    sd.circle(center_position=point, radius=radius, color=COLOR_WHITE, width=1)
 
 # Написать функцию рисования пузырька, принимающую 2 (или более) параметра: точка рисовании и шаг
 # см. выше
 # Нарисовать 10 пузырьков в ряд
 for x in range(100, 1001, 100):
-    point = sd.get_point(x, 100)
-    puzyirik(point=point, step=5, colour=(0, 250, 0))
+    point = sd.get_point(x, 450)
+    puzyirik(point=point, step=5, colour=(255, 255, 255))
+
 # Нарисовать три ряда по 10 пузырьков
 for y in range(100, 301, 100):
     for x in range(100, 1001, 100):
         point = sd.get_point(x, y)
-        puzyirik(point=point, step=5, colour=(0, 250, 0))
+        puzyirik(point=point, step=5, colour=(255, 255, 255))
+
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 for m in range(100):
     point = sd.random_point()
     colour = sd.random_color()
     puzyirik(point=point, step=5, colour=colour)
 sd.pause()
+
