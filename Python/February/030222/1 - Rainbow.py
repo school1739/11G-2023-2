4a9c659d7e694a11cd90cdf46f62b04f8c4ee591
LenovoUser <1>
2022-02-03 12:45:00 +0300

    почти сделал 1

---
+++ Python/February/030222/1 - Rainbow.py
@@ -0,0 +1,14 @@
+# (цикл for)
+
+import simple_draw as sd
+
+rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
+                  sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+point=sd.get_point(1000. -7)
+
+a=300
+b=0
+import time
+
+for i in range (300. 1000.-1)
+time.simple(0.3)
