9cc3abffc64e71c375eb2b1924332fbef11469b0
LenovoUse <anikanov.fedor.01@gmail.com>
2022-02-03 12:31:18 +0300

    задание 1

--- Python/February/030222/1 - Rainbow.py
+++ Python/February/030222/1 - Rainbow.py
@@ -2,35 +2,36 @@
 
 import simple_draw as sd
 
-'''rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
-                  sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)'''
+rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
+                  sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 # TODO здесь ваш код
 # Подсказка: цикл нужно делать сразу по кортежу с цветами радуги.
-def raduga ( step):
-point = sd.start_point(50,50)
-point1 = sd.end_point(350,450)
-radius=10
-step=5
-rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
-                  sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
-COLOR_RED = (255, 0, 0),
-COLOR_ORANGE = (255, 127, 0),
-COLOR_YELLOW = (255, 255, 0),
-COLOR_GREEN = (0, 255, 0),
-COLOR_CYAN = (0, 255, 255),
-COLOR_BLUE = (0, 0, 255),
-COLOR_PURPLE = (255, 0, 255)
-for i in range (7):
-    color+=color
-    radius+=step
-    sd.line(start_point=point, end_point=point1 , color=rainbow_colors, width=4,radius=radius):
-    sd.pause()
+start_x = 50
+start_y = 50
+end_x = 350
+end_y = 450
+step_x = 5
+step_y = 5
+
+for i in range(7):
+    start_x += i * step_x
+    start_y += i * step_y
+    end_x += i * step_x
+    end_y += i * step_y
+    sd.line(start_point=sd.get_point(start_x, start_y), end_point=sd.get_point(end_x, end_y), color=rainbow_colors[i],
+            width=4)
+
 
 
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
 # TODO здесь ваш код
 
+point=sd.get_point(5, 5)
+radius=50
+for i in range(7):
+    radius+=5
+    sd.circle(center_position=point, radius=radius, color=rainbow_colors[i], width=5)
 sd.pause()
