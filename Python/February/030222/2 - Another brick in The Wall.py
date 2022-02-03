0a8500f607acb4879ee16ac82b1527657f7d212b
LenovoUser <lady.khodina@yandex.ru>
2022-02-03 12:53:48 +0300

    ex.2...

--- Python/February/030222/2 - Another brick in The Wall.py
+++ Python/February/030222/2 - Another brick in The Wall.py
@@ -1,5 +1,5 @@
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
@@ -14,5 +14,12 @@ import simple_draw
 #       цикл координате X
 #           вычисляем правый нижний и левый верхний углы кирпича
 #           рисуем кирпич
+sd.set_screen_size(500,500)
+point=[0,50]
+for i in range(10):
+    left_bottom=sd.get_point(point[0]-35, point[1]-15)
+    right_top=sd.get_point(point[0]+35, point[1]+15)
+    sd.rectangle(left_bottom,right_top, color=(127,127,0), width=0)
+    point[0]+=70
 
-simple_draw.pause()
+sd.pause()
