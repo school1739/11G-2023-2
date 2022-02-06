8ec5176b1cb59ee219a72f62c410bc7b2186c4f6
ladyk <lady.khodina@yandex.ru>
2022-02-06 16:45:14 +0300

    ex.2

--- Python/February/030222/2 - Another brick in The Wall.py
+++ Python/February/030222/2 - Another brick in The Wall.py
@@ -1,25 +1,30 @@
-# (цикл for)
+### Задание №2 "Another brick in The Wall"
+##
+#
 import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
-# Использовать вложенные циклы for
 
-# TODO здесь ваш код
-# Подсказки:
-#  Для отрисовки кирпича использовать функцию rectangle
-#  Алгоритм должен получиться приблизительно такой:
-#
-#   цикл по координате Y
-#       вычисляем сдвиг ряда кирпичей
-#       цикл координате X
-#           вычисляем правый нижний и левый верхний углы кирпича
-#           рисуем кирпич
-sd.set_screen_size(500,500)
-point=[0,50]
-for i in range(10):
-    left_bottom=sd.get_point(point[0]-35, point[1]-15)
-    right_top=sd.get_point(point[0]+35, point[1]+15)
-    sd.rectangle(left_bottom,right_top, color=(127,127,0), width=0)
-    point[0]+=70
+sd.set_screen_size(500, 500)
+# первый вид рядов кирпичей
+for i in range(55, 501, 110):
+    for m in range(50, 501, 100):
+        point = [m, i]
+        left_bottom_2 = sd.get_point(point[0] - 55, point[1] - 30)
+        right_top_2 = sd.get_point(point[0] + 55, point[1] + 30)
+        sd.rectangle(left_bottom_2, right_top_2, color=(127, 127, 0), width=0)  # как бы слой цемента
+        left_bottom = sd.get_point(point[0] - 50, point[1] - 25)
+        right_top = sd.get_point(point[0] + 50, point[1] + 25)
+        sd.rectangle(left_bottom, right_top, color=(127, 63, 0), width=0)  # сами кирпичи
+# второй вид рядов кирпичей
+for i in range(0, 501, 110):
+    for m in range(0, 501, 100):
+        point = [m, i]
+        left_bottom_2 = sd.get_point(point[0] - 55, point[1] - 30)
+        right_top_2 = sd.get_point(point[0] + 55, point[1] + 30)
+        sd.rectangle(left_bottom_2, right_top_2, color=(127, 127, 0), width=0)  # как бы слой цемента
+        left_bottom = sd.get_point(point[0] - 50, point[1] - 25)
+        right_top = sd.get_point(point[0] + 50, point[1] + 25)
+        sd.rectangle(left_bottom, right_top, color=(127, 63, 0), width=0)  # сами кирпичи
 
 sd.pause()
