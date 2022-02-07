50f998640267aeda03798b2b16c7b28d18f8b63f
Kayutkin <glebstar28@gmail.com>
2022-02-07 12:50:34 +0300

    the game......

--- Python/The Game/main.py
+++ Python/The Game/main.py
@@ -11,7 +11,50 @@
 один из игроков не наберёт 50 очков, но не более 100 раундов."""
 
 import random
+
+p1_number = 0
+p2_number = 0
+
+
 def p1():
-    p1_number=random.randint(-1000,1000)
+    global p1_number
+    p1_number = random.randint(-1000, 1000)
+    return p1_number
+
+
 def p2():
-    p2_number=random.randint(-1000,1000)
+    global p2_number
+    p2_number = random.randint(-1000, 1000)
+    return p2_number
+
+
+def judge():
+    global p1_number
+    global p2_number
+    count_1 = 0
+    count_2 = 0
+    rounds = 0
+    while rounds <=100 or count_1 <= 50 or count_2 <= 50:
+        if p1_number == p2_number:
+            count_1 += 1
+            count_2 += 1
+        elif p1_number > p2_number:
+            count_1 += 1
+            count_2 -= 1
+        else:
+            count_1 -= 1
+            count_2 += 1
+        print("Первый игрок: ", count_1)
+        print("Второй игрок: ", count_2)
+        rounds += 1
+    if count_1 == count_2:
+        print("Победила дружба")
+    elif count_2 < count_1:
+        print("Победил первый игрок")
+    else:
+        print("Победил второй игрок")
+
+
+p1()
+p2()
+judge()
