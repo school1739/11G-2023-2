c24bb80867ebe8a87ebe915e85d1b2c395c143b3
Soviet_Sherlock <savvabaryshev@gmail.com>
2022-02-07 12:17:58 +0300

    сделав этот ваш Центор

---
+++ Python/January/240122/4 - Center.py
@@ -0,0 +1,24 @@
+### Задание №4 "Center"
+##
+#
+import random
+
+
+# функция для выводы строки нужным кол-вом пробелов
+def stroka(sp, wn):
+   space = " "
+   spaces = ""
+   # проверяем условие: длина строки больше размера окна
+   if len(sp) >= wn:
+      print(s)
+   else:
+      for i in range((wn - len(sp)) // 2):
+         spaces += space
+      print(spaces + sp)  # вывод результата
+
+
+# пример вывода нескольких строк в окнах разной ширины
+for _ in range(5):
+   a = "Привет! Как дела?"
+   b = random.randint(1, 500)
+   stroka(a, b)
