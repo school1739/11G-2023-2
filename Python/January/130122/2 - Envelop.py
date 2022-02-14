9d263f7ab4df1868253e654e23dae24dc2c37384
AnnaH <lady.khodina@yandex.ru>
2022-02-14 12:20:12 +0300

    clean

--- Python/January/130122/2 - Envelop.py
+++ Python/January/130122/2 - Envelop.py
@@ -1,49 +1,46 @@
-#1
-print("Введите параметры конверта")
-a = int(input())
-b = int(input())
-print("Введите параметры бумаги")
-c = int(input())
-d = int(input())
-if (a >= c and b >= d) or (a >= d and b >= c):
-    print("для введенных данных ДА")
-else:
-    print("для введенных данных НЕТ")
-c = [8, 9, 6, 8, 3, 11, 9]
-d = [9, 8, 8, 6, 4, 9, 11]
-for i in range(len(c)):
-    if (a >= c[i] and b >= d[i]) or (a >= d[i] and b >= c[i]):
-        print("для данных", c[i], d[i], "ДА")
-    else:
-        print("для данных", c[i], d[i], "НЕТ")
+# Заданы размеры envelop_x, envelop_y - размеры конверта и paper_x, paper_y листа бумаги
+#
+# Определить, поместится ли бумага в конверте (стороны листа параллельны сторонам конверта)
+#
+# Результат проверки вывести на консоль (ДА/НЕТ)
+# Использовать только операторы if/elif/else, можно вложенные
 
-#2
+# АВТОМАТИЧЕСКИ проверить для:
+# paper_x, paper_y = 8, 9
+# paper_x, paper_y = 9, 8
+# paper_x, paper_y = 6, 8
+# paper_x, paper_y = 8, 6
+# paper_x, paper_y = 3, 4
+# paper_x, paper_y = 11, 9
+# paper_x, paper_y = 9, 11
+# (написать цикл для проверки)
 
-print("Введите параметры отверстия")
-a = int(input())
-b = int(input())
-print("Введите параметры кирпича")
-c = int(input())
-d = int(input())
-e = int(input())
-if (a >= c and b >= d) or (a >= c and b >= e) or (a >= d and b >= c) or (a >= d and b >= e) or (a >= e and b >= c) or (
-        a >= e and b >= d):
-    print("для введенных данных ДА")
-else:
-    print("для введенных данных НЕТ")
-c = [11, 11, 10, 10, 2, 2, 3, 3, 6, 6, 5, 5, 11, 11, 6, 6, 3, 3]
-d = [10, 2, 11, 2, 10, 11, 5, 6, 3, 5, 6, 3, 3, 6, 11, 3, 6, 11]
-e = [2, 10, 2, 11, 11, 10, 6, 5, 5, 3, 3, 6, 6, 3, 3, 11, 11, 6]
-for i in range(len(c)):
-    if (a >= c[i] and b >= d[i]) or (a >= c[i] and b >= e[i]) or (a >= d[i] and b >= c[i]) or (
-            a >= d[i] and b >= e[i]) or (
-            a >= e[i] and b >= c[i]) or (
-            a >= e[i] and b >= d[i]):
-        print("для данных", c[i], d[i], e[i],
-        "ДА")
-    else:
-        print("для данных", c[i], d[i], e[i],
-        "НЕТ")
+# TODO здесь ваш код
 
-# +-OK. Зачем вводить параметры конверта\кирпича, если всё равно проверка по
-# заданному списку?
+# Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
+# могут быть в диапазоне от 1 до 1000)
+#
+# Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
+
+# АВТОМАТИЧЕСКИ проверить для:
+# brick_x, brick_y, brick_z = 11, 10, 2
+# brick_x, brick_y, brick_z = 11, 2, 10
+# brick_x, brick_y, brick_z = 10, 11, 2
+# brick_x, brick_y, brick_z = 10, 2, 11
+# brick_x, brick_y, brick_z = 2, 10, 11
+# brick_x, brick_y, brick_z = 2, 11, 10
+# brick_x, brick_y, brick_z = 3, 5, 6
+# brick_x, brick_y, brick_z = 3, 6, 5
+# brick_x, brick_y, brick_z = 6, 3, 5
+# brick_x, brick_y, brick_z = 6, 5, 3
+# brick_x, brick_y, brick_z = 5, 6, 3
+# brick_x, brick_y, brick_z = 5, 3, 6
+# brick_x, brick_y, brick_z = 11, 3, 6
+# brick_x, brick_y, brick_z = 11, 6, 3
+# brick_x, brick_y, brick_z = 6, 11, 3
+# brick_x, brick_y, brick_z = 6, 3, 11
+# brick_x, brick_y, brick_z = 3, 6, 11
+# brick_x, brick_y, brick_z = 3, 11, 6
+# (написать цикл для проверки)
+
+# TODO здесь ваш код
