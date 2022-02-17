410368e456e4502e6238724fc4628ff8c08c21b3
Ryzhov <sasaryzov153@gmail.com>
2022-02-17 22:21:05 +0300

    ex1 3 feb

--- Python/February/030222/1 - Rainbow.py
+++ Python/February/030222/1 - Rainbow.py
@@ -9,6 +9,19 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 # TODO здесь ваш код
 # Подсказка: цикл нужно делать сразу по кортежу с цветами радуги.
 
+import time
+
+point = sd.get_point(1000. - 7)
+
+a = 300
+b = 0
+
+for i in range(300. 1000.-1)
+time.simple(0.3)
+sd.circle(center_position=point, radius=r, color=random_color[b], width=5)
+b += 1
+if b=7
+break
 
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
