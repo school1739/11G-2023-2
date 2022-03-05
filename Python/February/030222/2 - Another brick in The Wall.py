d29bc1b1b3bff7bd71daf83fc747c6af86d0d084
Student <>
2022-03-05 12:21:07 +0300

    переделал все на 3 и игру

--- Python/February/030222/2 - Another brick in The Wall.py
+++ Python/February/030222/2 - Another brick in The Wall.py
@@ -4,23 +4,50 @@ import simple_draw
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
-brick_size = (100, 50)
-background_color = (100,) * 3
-space_width = 10
-rows = 10
-bricks_in_row = 10
+# установка размеров окна и заголовка
+screen_w = 640
+screen_h = 480
+simple_draw.caption = "Wall"
+simple_draw.set_screen_size(screen_w, screen_h)
 
-sd.rectangle(sd.get_point(0, 0), sd.get_point(*sd.resolution), background_color)
-for x in range(bricks_in_row):
-for y in range(rows):
-brick_x = (brick_size[0] + space_width) * x
-brick_y = (brick_size[1] + space_width) * y
-if y % 2 == 1:
-brick_x -= (brick_size[0] + space_width) / 2
-start = sd.get_point(brick_x, brick_y)
-end = sd.get_point(brick_x + brick_size[0], brick_y + brick_size[1])
-sd.rectangle(start, end, sd.COLOR_ORANGE)
+
+# функция отрисовки кирпича
+def brick(x, y, w, h):
+    # координаты вершин
+    p1 = simple_draw.get_point(x, y)
+    p2 = simple_draw.get_point(x + w, y + h)
+    # заливка
+    simple_draw.rectangle(p1, p2, simple_draw.COLOR_DARK_RED, 0)
+    # границы (соединения кирпичей)
+    simple_draw.rectangle(p1, p2, simple_draw.COLOR_WHITE, 4)
+
+
+# размеры кирпича
+brick_w = 100
+brick_h = 50
+
+# варианты начального положения ряда кирпичей
+# нечётный ряд рисуется с координаты X = 0
+# чётный - X = -половина ширины кирпича
+brick_start_x = [0, -brick_w // 2]
+brick_start_x_idx = 0   # индекс текущего варианта
+
+brick_y = 0     # Y текущего рядя кирпичей
+# в цикле по Y до высоты окна
+while brick_y < screen_h:
+    # задаём начальный X кирпича в соответствии с вариантом
+    brick_x = brick_start_x[brick_start_x_idx]
+
+    # в цикле по X до ширины окна рисуем кирпич и смещаемся на ширину кирпича
+    while brick_x < screen_w:
+        brick(brick_x, brick_y, brick_w, brick_h)
+        brick_x += brick_w
+
+    # смещаем ряд на высоту
+    brick_y += brick_h
+    # меняем вариант начальной координаты X
+    brick_start_x_idx = not brick_start_x_idx
+
 
 # Подсказки:
 #  Для отрисовки кирпича использовать функцию rectangle
--- Python/February/030222/3 - Shit happens.py
+++ Python/February/030222/3 - Shit happens.py
@@ -6,91 +6,163 @@ import simple_draw
 # Параметры функции: кордината X, координата Y, цвет.
 # Под смайликом сделать подпись "Shit happens"
 
-# TODO здесь ваш код
-# Ввод словаря со списками точек начала и конца линий каждой буквы
-symbols = {
-'s': [((1, 0.8), (0.5, 1)),
-((0.5, 1), (0, 0.7)),
-((0, 0.7), (1, 0.3)),
-((1, 0.3), (0.8, 0)),
-((0.8, 0), (0, 0.3))],
-'h': [((0, 1), (0, 0)),
-((0, 0.5), (1, 0.5)),
-((1, 0.5), (1, 0))],
-'i': [((0.5, 0), (0.5, 0.8)),
-((0.5, 0.9), (0.5, 1))],
-'t': [((0, 1), (1, 1)),
-((0.5, 1), (0.5, 0))],
-'a': [((0, 0.7), (0.5, 1)),
-((0.5, 1), (1, 0.7)),
-((1, 0.7), (1, 0)),
-((1, 0.5), (0, 0.5)),
-((0, 0.5), (0, 0)),
-((0, 0), (1, 0))],
-'p': [((0, 0), (0, 1)),
-((0, 1), (1, 1)),
-((1, 1), (1, 0.5)),
-((1, 0.5), (0, 0.5))],
-'e': [((1, 0.3), (0.5, 0)),
-((0.5, 0), (0, 0.3)),
-((0, 0.3), (0, 0.7)),
-((0, 0.7), (0.5, 1)),
-((0.5, 1), (1, 0.5)),
-((1, 0.5), (0, 0.5))],
-'n': [((0, 0), (0, 1)),
-((0, 0.8), (0.5, 1)),
-((0.5, 1), (1, 0.8)),
-((1, 0.8), (1, 0))],
-' ': []
+
+# структура, содержащая информацию о координатах линий заданных символов
+# координаты относительные (от 0 до 1)
+glyphs_info = {
+    's': [
+        ((0, 0), (1, 0)),
+        ((1, 0), (1, 0.5)),
+        ((1, 0.5), (0, 0.5)),
+        ((0, 0.5), (0, 1)),
+        ((0, 1), (1, 1))
+    ],
+    'h': [
+        ((0, 0), (0, 1)),
+        ((1, 0), (1, 1)),
+        ((0, 0.5), (1, 0.5))
+    ],
+    'i': [
+        ((0.5, 0), (0.5, 1))
+    ],
+    't': [
+        ((0.5, 0), (0.5, 1)),
+        ((0, 1), (1, 1))
+    ],
+    'a': [
+        ((0, 0), (0.5, 1)),
+        ((0.5, 1), (1, 0)),
+        ((0.25, 0.5), (0.75, 0.5))
+    ],
+    'p': [
+        ((0, 0), (0, 1)),
+        ((0, 1), (1, 1)),
+        ((1, 1), (1, 0.5)),
+        ((1, 0.5), (0, 0.5))
+    ],
+    'e': [
+        ((0, 0), (0, 1)),
+        ((0, 1), (1, 1)),
+        ((0, 0.5), (1, 0.5)),
+        ((0, 0), (1, 0))
+    ],
+    'n': [
+        ((0, 0), (0, 1)),
+        ((1, 0), (1, 1)),
+        ((0, 1), (1, 0))
+    ]
 }
 
-sd.background_color = sd.COLOR_GREEN
-sd.set_screen_size(1000, 600)
-smile_radius = 200
 
-# Ввод функция для смайлика
-def draw_smile(x, y):
-# Круг
-center = sd.get_point(x, y)
-sd.circle(center, smile_radius, sd.COLOR_YELLOW, 0)
-# Улыбка с помощью двух эллипсов, один перекрывает другой
-sd.ellipse(left_bottom=sd.get_point(x - 125, y - 150), right_top=sd.get_point(x + 125, y), color=sd.COLOR_BLACK,
-width=0)
-sd.ellipse(left_bottom=sd.get_point(x - 125, y - 125), right_top=sd.get_point(x + 125, y + 25),
-color=sd.COLOR_YELLOW,
-width=0)
-sd.circle(center, smile_radius // 6, sd.COLOR_YELLOW, 2)
-# Глаза, представляющие собой два круга
-eyes_y = center.y + smile_radius // 3
-eyes_shift_x = smile_radius // 2
-eyes_radius = smile_radius // 6
-sd.circle(sd.get_point(center.x - eyes_shift_x, eyes_y), eyes_radius, sd.COLOR_RED, 0)
-sd.circle(sd.get_point(center.x + eyes_shift_x, eyes_y), eyes_radius, sd.COLOR_RED, 0)
+# функция отрисовка смайлика
+# x, y - координаты центра смайлика
+# color - цвет заливки смайлика
+# r - радиус круга смайлика
+def smile_icon(x, y, color, r=100):
+    pos = simple_draw.get_point(x, y)   # центр круга смайлика
+    # заливка
+    simple_draw.circle(pos, r, color, 0)
+    # граница
+    simple_draw.circle(pos, r, simple_draw.COLOR_BLACK, 4)
 
-# Функция для рисования символа
-def draw_symbol(symbol, start, width, height, thickness):
-# Рисуем каждую линию
-for line in symbols[symbol]:
-line_start = sd.get_point(line[0][0] * width + start.x, line[0][1] * height + start.y)
-line_end = sd.get_point(line[1][0] * width + start.x, line[1][1] * height + start.y)
-sd.line(line_start, line_end, sd.COLOR_WHITE, thickness)
+    # координаты и размеры прищуренных глаз (прямая линия)
+    eye_w = r / 3                   # ширина глаза
+    eye_y = y + r / 3               # координата Y глаза
+    eye1_x = x - r / 2 - eye_w / 2  # координата X левого конца левого глаза
+    eye2_x = x + r / 2 - eye_w / 2  # координата X левого конца правого глаза
 
-# Функция для рисования текста
-def draw_text(text, left_bottom, right_top, thickness):
-# Рассчитываем общую ширину и ширину и высоту символов
-width = right_top.x - left_bottom.x
-height = right_top.y - left_bottom.y
-symbol_width = width // len(text)
-# Рисуем каждый символ
-for i, symbol in enumerate(text):
-# Увеличиваем высоту символа в 2 раза если это заглавная буква
-symbol_height = height
-if symbol.isupper():
-symbol_height *= 2
-draw_symbol(symbol.lower(), sd.get_point(left_bottom.x + symbol_width * i, left_bottom.y),
-symbol_width // 2, symbol_height, thickness)
+    # концы отрезка левого глаза
+    eye1_p1 = simple_draw.get_point(eye1_x, eye_y)
+    eye1_p2 = simple_draw.get_point(eye1_x + eye_w, eye_y)
+    # рисуем левый глаз
+    simple_draw.line(eye1_p1, eye1_p2, simple_draw.COLOR_BLACK, 2)
 
-draw_smile(450, 400)
-draw_text('Shit happens', sd.get_point(50, 50), sd.get_point(1000, 100), 5)
+    # концы отрезка правого глаза
+    eye2_p1 = simple_draw.get_point(eye2_x, eye_y)
+    eye2_p2 = simple_draw.get_point(eye2_x + eye_w, eye_y)
+    # рисуем правый глаз
+    simple_draw.line(eye2_p1, eye2_p2, simple_draw.COLOR_BLACK, 2)
+
+    # координаты и размеры рта (прямая линия)
+    mouth_w = r
+    mouth_x = x - mouth_w / 2
+    mouth_y = y - r / 3
+
+    # концы отрезка рта
+    mouth_p1 = simple_draw.get_point(mouth_x, mouth_y)
+    mouth_p2 = simple_draw.get_point(mouth_x + mouth_w, mouth_y)
+    # рисуем рот
+    simple_draw.line(mouth_p1, mouth_p2, simple_draw.COLOR_BLACK, 2)
+
+
+# функция отрисовка символа
+# glyph - символ
+# x, y - положение символа (левый нижний угол)
+# w, h - размеры символа
+# color - цвет символа
+def draw_glyph(glyph, x, y, w, h, color):
+    # рисуем только, если есть информация о символе
+    if glyph in glyphs_info.keys():
+        # массив кортежей с координатами концов отрезков
+        glyph_lines = glyphs_info[glyph]
+        # в цикле по отрезкам
+        for line in glyph_lines:
+            # определяем координаты концов отрезков в соответсвие с заданными
+            # координатами на холсте и размерами символа
+            p1 = line[0]
+            p2 = line[1]
+            x1 = x + p1[0] * w
+            y1 = y + p1[1] * h
+            x2 = x + p2[0] * w
+            y2 = y + p2[1] * h
+            # формируем точки
+            draw_p1 = simple_draw.get_point(x1, y1)
+            draw_p2 = simple_draw.get_point(x2, y2)
+            # рисуем
+            simple_draw.line(draw_p1, draw_p2, color, 1)
+
+
+# функция отрисовка текста
+# txt - строка
+# x, y - положение текста (левый нижний угол)
+# w, h - размеры прямоугольника, в который вписан текст
+# color - цвет текста
+def draw_text(txt, x, y, w, h, color):
+    space_w = max(1, w // 25)   # межсимвольное расстояние
+    txt_len = len(txt)          # длина строки
+    glyph_w = (w - space_w * (txt_len - 1)) / txt_len   # ширина символа
+    # в цикле по символам рисуем символ, смещая положение символа по X на
+    # ширину символа и межсимвольное расстояние
+    for glyph in txt:
+        glyph_h = h     # высота символа (увеличить на 50%, если заглавный)
+        if glyph.isupper():
+            glyph_h += glyph_h // 2
+
+        draw_glyph(glyph.lower(), x, y, glyph_w, glyph_h, color)
+        x += glyph_w + space_w
+
+
+# установка размеров окна и заголовка
+screen_w = 640
+screen_h = 480
+simple_draw.caption = "Smile"
+simple_draw.set_screen_size(screen_w, screen_h)
+
+# параметры смайлика
+smile_x = screen_w // 2
+smile_y = screen_h // 2 + 64
+smile_r = 100
+
+smile_icon(smile_x, smile_y, simple_draw.COLOR_YELLOW, smile_r)
+
+# параметры текста
+txt_h = 32
+txt_w = screen_w * 3 // 4
+txt_x = smile_x - txt_w // 2
+txt_y = smile_y - smile_r // 2 - txt_h - 64
+
+draw_text("Shit happens", txt_x, txt_y, txt_w, txt_h, simple_draw.COLOR_WHITE)
 
 simple_draw.pause()
+
+# OK
