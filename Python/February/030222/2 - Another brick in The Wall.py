f3928e9a52f58674f3f31e6b52913e4279207741
Student <anikanov.fedor.01@gmail.com>
2022-03-05 12:30:05 +0300

    переделал 2 и 3 номера

--- Python/February/030222/2 - Another brick in The Wall.py
+++ Python/February/030222/2 - Another brick in The Wall.py
@@ -4,7 +4,51 @@ import simple_draw
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+# установка размеров окна и заголовка
+screen_w = 640
+screen_h = 480
+simple_draw.caption = "Wall"
+simple_draw.set_screen_size(screen_w, screen_h)
+
+
+# функция отрисовки кирпича
+def brick(x, y, w, h):
+    # координаты вершин
+    p1 = simple_draw.get_point(x, y)
+    p2 = simple_draw.get_point(x + w, y + h)
+    # заливка
+    simple_draw.rectangle(p1, p2, simple_draw.COLOR_DARK_RED, 0)
+    # границы (соединения кирпичей)
+    simple_draw.rectangle(p1, p2, simple_draw.COLOR_WHITE, 4)
+
+
+# размеры кирпича
+brick_w = 100
+brick_h = 50
+
+# варианты начального положения ряда кирпичей
+# нечётный ряд рисуется с координаты X = 0
+# чётный - X = -половина ширины кирпича
+brick_start_x = [0, -brick_w // 2]
+brick_start_x_idx = 0   # индекс текущего варианта
+
+brick_y = 0     # Y текущего рядя кирпичей
+# в цикле по Y до высоты окна
+while brick_y < screen_h:
+    # задаём начальный X кирпича в соответствии с вариантом
+    brick_x = brick_start_x[brick_start_x_idx]
+
+    # в цикле по X до ширины окна рисуем кирпич и смещаемся на ширину кирпича
+    while brick_x < screen_w:
+        brick(brick_x, brick_y, brick_w, brick_h)
+        brick_x += brick_w
+
+    # смещаем ряд на высоту
+    brick_y += brick_h
+    # меняем вариант начальной координаты X
+    brick_start_x_idx = not brick_start_x_idx
+
+
 # Подсказки:
 #  Для отрисовки кирпича использовать функцию rectangle
 #  Алгоритм должен получиться приблизительно такой:
@@ -15,26 +59,4 @@ import simple_draw
 #           вычисляем правый нижний и левый верхний углы кирпича
 #           рисуем кирпич
 
-# (цикл for)
-import simple_draw as sd
-
-# Нарисовать стену из кирпичей. Размер кирпича - 100х50
-# Использовать вложенные циклы for
-
-brick_size = (100, 50)
-background_color = (100,) * 3
-space_width = 10
-rows = 10
-bricks_in_row = 10
-
-sd.rectangle(sd.get_point(0, 0), sd.get_point(*sd.resolution), background_color)
-for x in range(bricks_in_row):
-    for y in range(rows):
-        brick_x = (brick_size[0] + space_width) * x
-        brick_y = (brick_size[1] + space_width) * y
-        if y % 2 == 1:
-            brick_x -= (brick_size[0] + space_width) / 2
-        start = sd.get_point(brick_x, brick_y)
-        end = sd.get_point(brick_x + brick_size[0], brick_y + brick_size[1])
-        sd.rectangle(start, end, sd.COLOR_ORANGE)
-sd.pause()
+simple_draw.pause()
