254c917bae4032c36e02f8b901bc6a733d6d3447
Student <anikanov.fedor.01@gmail.com>
2022-03-14 12:44:26 +0300

    111

---
+++ Python/January/new.py
@@ -0,0 +1,12 @@
+class Player:
+    def say(self, message):
+        print(message)
+#
+# Ivanov = Player()
+# Petrov = Player()
+#
+# Ivanov.say()
+# Petrov.say()
+
+    def sayShit(self):
+        self.say("Shit!")
