cbf1962b98b471da5a0532e472109acc40f79df7
Student <lady.khodina@yandex.ru>
2022-03-21 12:45:43 +0300

    class

--- Python/RIP.py
+++ Python/RIP.py
@@ -1,11 +1,37 @@
+import random
 class Player:
     def say(self, message):
         print(message)
     def sayShit(self):
         self.say("Пошёл ты!")
+    def __init__(self, name):
+        print ('Создан игрок')
+        self.name = name
+        self.age = random.randint(12,55)
+class Player_inCapsule:
+    def __init__(self, name, age):
+        print("Создан инкапусированный игрок")
+        self.__name=name
+        self.__age=age
+    def say_name(self):
+        return self.__name
+    def say_age(self):
+        return  self.__age
 
 
-Ivanov=Player()
-Petrov=Player()
-Ivanov.say("Дестиэль канон!")
-Petrov.sayShit()
+Ivanov=Player("Inanoff")
+Petrov=Player("Petroff")
+# Ivanov.say("Дестиэль канон!")
+# Petrov.sayShit()
+
+print(f"Player name: {Ivanov.name} Player age: {Ivanov.age}" )
+print(f"Player name: {Petrov.name}     Player age: {Petrov.age}")
+Ivanov.name="Was Ivanoff once"
+Ivanov.age=78
+Semenov=Player_inCapsule("Semion", 55)
+# print(f"Player name: {Semenov.__name} Player age: {Semenov.__age}")
+print(Semenov.say_age())
+print(Semenov.say_name())
+
+
+
