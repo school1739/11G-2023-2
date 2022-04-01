4015b97952689bd41d049dda707bdf996bbb8d30
Bormotoon <bormotoon@gmail.com>
2022-04-01 11:45:53 +0300

    Shitty Vigenere init.

--- Info Sec/Chiffre de Vigenère.py
+++ Info Sec/Chiffre de Vigenère.py
@@ -12,3 +12,19 @@ alphabet_EU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
 #       Ci=(Pi+Ki) mod 26
 #   Расшифровка:
 #       Pi=(Ci-Ki+26) mod 26
+# C = Cipher Text (зашифрованный текст)
+# P = Plain Text (изначальный текст)
+# K = Key (ключ)
+
+plain_text = input("PLAIN: ")
+key_text = input("KEY: ")
+for key_char in key_text:
+    key_index = alphabet_RU.find(key_char)
+    print(key_index + 1)
+
+for plain_char in plain_text:
+    plain_index = alphabet_RU.find(plain_char)
+    print(plain_index + 1)
+
+for char in len(plain_text):
+    print(alphabet_RU[plain_index+key_index])
