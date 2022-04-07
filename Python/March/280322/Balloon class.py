9e29725ed1c618b0b5d48b25009f3fb500c2f887
AV3 <>
2022-04-07 17:50:10 +0300

    долги

--- Python/March/280322/Balloon class.py
+++ Python/March/280322/Balloon class.py
@@ -4,7 +4,7 @@ import random
 """
 Задача: написать программу, выводящую на экран праздничный салют
 и воздушные шарики, используя классы.
-Салют: 5 групп по 6 фейерверков (1 большой в центре, 5 в два раза
+Салюты (5 салютов): 6 фейерверков (1 большой в центре, 5 в два раза
 меньших вокруг). Фейерверк: не менее 15 закрашенных окружностей,
 расположенных группой.
 Шарик: основной овал, треугольник-пуцка, ломанная линия (не менее
@@ -12,35 +12,60 @@ import random
 Фейерверки и шарики описываются в классах (обратите внимание на наследование!).
 Постарайтесь максимально часто использовать рандом с небольшим разбросом,
 чтобы не было одинаковых фигур.
-
 Использование предопределенных классов обязательно. Дополнительные классы,
-инкапсуляция, функции и классовые методы -- произвольно.
+инкапсуляция, функции и классовые методы - произвольно.
 Использование pygame и/или иных библиотек, кроме SD и random не допускается.
 """
+width = 1500
+height = 700
+sd.set_screen_size(width, height)
+sd._init()
 
-class Firework: # Основной класс фейерверка
-    def __init__(self):
-        pass
 
-class FireworkCenter(Firework): # Подкласс для большой центральной части фейерверка
-    pass
-    def draw_1(self):
-        #colors =  ['red' 'blue' 'yellow']
-        center_pos = sd.get_point(random.randint(0, 500), random.randint(0, 500))
-        self.center_pos = center_pos
-        COLOR_WHITE = (255, 255, 255)
-        sd.circle(center_position=center_pos, radius=60, color=COLOR_WHITE, width=0)
-fare = FireworkCenter()
-fare.draw_1()
-class FireworkAround(Firework): # Подкласс для маленьких фейерверков вокруг основного
-    def s(self):
-        COLOR_ORANGE = (255, 127, 0)
-        center_pos = sd.get_point(random.randint(300, 500), random.randint(300, 500))
-        for i in range(5):
-            center_pos = sd.get_point(random.randint(300, 500), random.randint(300, 500))
-            sd.circle(center_position=center_pos, radius=60, color=COLOR_ORANGE, width=0)
-s = FireworkAround()
-s.s()
-class Balloon: # Класс для шарика
-    pass
+center = sd.get_point(width / 2, height / 2)
+
+
+class Firework:
+    def fireworks(x, y):
+        sd.circle(sd.get_point(x, y), 10, sd.random_color(), 0)
+
+
+
+
+class FireworkCenter(Firework):
+    for i in range(1000):
+        Firework.fireworks(center.x + random.randint(-150, 150), center.y + random.randint(-150, 150))
+
+
+
+class FireworkAround(Firework):
+    for indent in range(3):
+        for i in range(300):
+            Firework.fireworks(center.x + random.randint(-450, -350) + 400 * indent,
+                               center.y + random.randint(250, 350))
+    for indent in range(2):
+        for i in range(300):
+            Firework.fireworks(center.x + random.randint(-250, -150) + 400 * indent,
+                               center.y + random.randint(-350, -250))
+
+
+
+class Balloon:
+    def balloon(point):
+        sd.ellipse(sd.get_point(point.x - 20, point.y - 30), sd.get_point(point.x + 20, point.y + 30),
+                   sd.random_color())
+
+        sd.polygon(point_list=[sd.get_point(point.x, point.y - 30), sd.get_point(point.x - 5, point.y - 40),
+                               sd.get_point(point.x + 5, point.y - 40)], color=sd.random_color(), width=0)
+
+        sd.line(sd.get_point(point.x, point.y - 40), sd.get_point(point.x - 5, point.y - 60))
+        sd.line(sd.get_point(point.x - 5, point.y - 60), sd.get_point(point.x + 5, point.y - 80))
+        sd.line(sd.get_point(point.x + 5, point.y - 80), sd.get_point(point.x - 5, point.y - 100))
+        sd.line(sd.get_point(point.x - 5, point.y - 100), sd.get_point(point.x + 5, point.y - 120))
+
+
+
+for i in range(10):
+    Balloon.balloon(sd.random_point())
+
 sd.pause()
---
+++ Python/March/280322/Glass Neural Network.py
@@ -0,0 +1,165 @@
+# Нейросеть из стаканов
+
+#Правила игры
+"""
+На столе 11 палочек, два игрока ходят по очереди. За каждый ход можно взять 1 или 2 палочки.
+Проиграл тот, кто взял последнюю палочку.
+
+Пользователь играет против компьютера. Первый ход определяется случайным образом, далее первым ходит игрок,
+проигравший в предыдущем раунде.
+
+Игра IRL:
+На столе стоят 10 стаканов (можно 11, но стакан для хода на последнюю палочку никогда не используется,
+т.к. в этом нет смысла). В каждом стакане ("нейроне") содержится по 5 бумажек с цифрами 1 и 2.
+Каждый свой ход "нейросеть" выбирает из стакана с номером, соответствующим числу палочек на столе,
+случайную бумажку с цифрой, затем берёт со стола количество палочек == числу на бумажке, затем передаёт
+ход игроку.
+
+В случае проигрыша "нейросети", из всех стаканов, которые принимали участие в раунде, убирается бумажка,
+которую "нейронка" выбрала для хода. В случае выигрыша - добавляется новая бумажка с тем же значением, что
+у использованной.
+
+Если в "нейроне" остаётся последняя бумажка любого значения, она не удаляется при следующей
+неудачной итерации -- на случай, если "нейросеть" попала в цикл неудачных решений на других "нейронах".
+Поэтому бумажка с неправильным решением удаляется только после 10 подряд неудачных раундов.
+Если несколько "нейронов" одновремено принимают неверные решения, удаляются не все сразу, а нейрон
+с минимальным номером, через 10 неудач -- следующий минимальный и т.д..
+
+Выполнение программы продолжается до 5 подряд побед "нейросети". После победы "нейросеть" показывает
+содержимое стаканов -- сколько и каких бумажек в них содержится, какова вероятность принятия того или
+иного решения в каждом "нейроне".
+
+Использование сторонних библиотек (pybrain, pygame, numpy и т.п.) запрещено. Можно использовать только random."""
+
+import enum
+import random
+import os
+
+class Neuron:
+
+    def __init__(self):
+        self.values = [1] * 5 + [2] * 5
+        self.last_value = 0
+        self.fails_in_row = 0
+
+
+    def choose(self):
+        self.last_value = random.choice(self.values)
+        return self.last_value
+
+
+    def fail(self):
+        assert self.last_value != 0
+        if len(self.values) > 1:
+            self.values.remove(self.last_value)
+        else:
+            self.fails_in_row += 1
+
+    def success(self):
+        assert self.last_value != 0
+        self.values.append(self.last_value)
+        self.fails_in_row = 0
+
+
+    def reset(self):
+        self.__init__()
+
+
+class AI:
+
+    def __init__(self):
+        self.neurons = [Neuron() for _ in range(10)]
+        self.used_neurons = []
+
+    def choose_number(self, sticks_left):
+        neuron = self.neurons[sticks_left - 2]
+        self.used_neurons.append(neuron)
+        return neuron.choose()
+
+    def fail(self):
+        for neuron in self.used_neurons:
+            neuron.fail()
+            if neuron.fails_in_row >= 10:
+                neuron.reset()
+        self.used_neurons.clear()
+
+
+    def win(self):
+        for neuron in self.used_neurons:
+            neuron.success()
+        self.used_neurons.clear()
+
+
+class Color(str, enum.Enum):
+    RESET = "\u001B[0m",
+    BLACK = "\u001B[30m",
+    RED = "\u001B[31m",
+    GREEN = "\u001B[32m",
+    YELLOW = "\u001B[33m",
+    BLUE = "\u001B[34m",
+    PURPLE = "\u001B[35m",
+    CYAN = "\u001B[36m",
+    WHITE = "\u001B[37m"
+
+
+def colored(value, color):
+    return color + str(value) + Color.RESET
+
+
+
+def player_turn():
+    choice = int(input('Сколько возьмёте палочек: 1 или 2?: '))
+    if choice not in (1, 2):
+        print(':с')
+        choice = player_turn()
+    return choice
+
+
+def bot_turn(ai, sticks_left):
+    choice = ai.choose_number(sticks_left)
+    print(f'Я возьму столько палочек: {colored(choice, Color.BLUE)}')
+    return choice
+
+def random_turn():
+    choice = random.randint(1, 2)
+    print(f'Рандомно выбрал число: {colored(choice, Color.YELLOW)}')
+    return choice
+
+bot = AI()
+
+random_play_rounds = int(input('Сколько раундов сыграть рандомно? (Можно 0): '))
+
+while True:
+    sticks_left = 11
+    turn = random.randint(0, 1)
+    random_play = False
+    if random_play_rounds > 0:
+        random_play = True
+        random_play_rounds -= 1
+
+    while sticks_left > 1:
+        print(f'Палочек осталось: {colored(sticks_left, Color.RED)}')
+        if turn == 0:
+            if random_play:
+                sticks_left -= random_turn()
+            else:
+                sticks_left -= player_turn()
+        else:
+            sticks_left -= bot_turn(bot, sticks_left)
+        turn = 1 - turn
+    print(colored('Осталась последняя палочка!', Color.CYAN))
+
+    if turn == 0:
+        print('Я победил')
+        bot.win()
+    else:
+        print('Ты победил')
+        bot.fail()
+    print('\nСтаканчики:')
+    print('1: Не используется')
+    for i, neuron in enumerate(bot.neurons):
+        chance_one = neuron.values.count(1) / len(neuron.values) * 100
+        print(f'{i + 2}: '
+              f'Шансы - {chance_one:.2f}%, {100 - chance_one:.2f}%;\t'
+              f'Бумажки - {sorted(neuron.values)}')
+    print()
