574ea8018e9f482927a6146b8c3bfad8929d1ea0
Bormotoon <bormotoon@gmail.com>
2022-04-13 12:53:51 +0300

    The Game 2.0 with classes: 5.

--- Python/The Game/game_classes.py
+++ Python/The Game/game_classes.py
@@ -1,10 +1,12 @@
 import random
 
-rounds = 0 #
+rounds = 0  #
+
+
 def cheater(cheat, count_cheat):
     CHEAT = False
 
-    if random.randint(5,100) <= cheat and count_cheat >= 3:
+    if random.randint(5, 100) <= cheat and count_cheat >= 3:
         CHEAT = True
     return CHEAT
 
@@ -15,25 +17,30 @@ count_2 = 0
 fails_1 = 0
 fails_2 = 0
 
+
 class Player:
     def __init__(self, name):
         self.__name = name
         self.cheat = 0
-        self.x=-1000
-        self.y=1000
+        self.x = -1000
+        self.y = 1000
+
     def say_name(self):
         return self.__name
+
     def number(self, fails):
         if fails == 3:
             self.cheat += 5
         if cheater(self.cheat, fails) is True:
-            self.x+=1000
-            self.y+=1000
+            self.x += 1000
+            self.y += 1000
         self.num = random.randint(self.x, self.y)
-        return self.num # вывод числа
+        return self.num  # вывод числа
+
 
 player_1 = Player("Dean")
 player_2 = Player("Sam")
+
 
 class Judge:
 
@@ -44,8 +51,8 @@ class Judge:
         if number_1 == number_2:
             count_1 += 1
             count_2 += 1
-            fails_1+=0
-            fails_2+=0
+            fails_1 += 0
+            fails_2 += 0
         elif number_1 > number_2:
             count_1 += 1
             count_2 -= 1
@@ -55,17 +62,20 @@ class Judge:
             count_2 += 1
             fails_1 += 1
 
-        print( player_1.say_name() ," : ", count_1)
-        print( player_2.say_name() ," : ", count_2)
+        print(player_1.say_name(), " : ", count_1)
+        print(player_2.say_name(), " : ", count_2)
+
 
 judge = Judge()
 
 while count_1 != 50 and count_2 != 50 and rounds != 100:
     judge.Game()
-    rounds+=1
+    rounds += 1
 if count_1 == count_2:
     print("Ничью")
 elif count_2 < count_1:
     print("Победил первый игрок")
 else:
     print("Победил второй игрок")
+
+# OK. Нет вывода текущей ситуации. Отдельный респект за игроков, хехе XD
