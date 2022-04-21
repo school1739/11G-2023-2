343369ec63600cc356c7171d2fc0ce2f45d4b454
Student <12354>
2022-04-21 13:25:19 +0300

    classwork

--- Info Sec/Chiffre de Vigenère.py
+++ Info Sec/Chiffre de Vigenère.py
@@ -6,75 +6,25 @@
 alphabet_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 alphabet_EU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-# Напишите программу, шифрующую входящее сообщение с помощью шифра Виженера.
-# Шифр Виженера представляет собой шифр Цезаря с переменной величиной сдвига.
-# Величину сдвига задают ключевым словом.
-# Например слово БАЗА означает последовательность сдвигов исходных букв: 219121912191...
-
-alphabet_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-alphabet_EU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 # HINT (использовать не обязательно):
 # Если буквы A-Z соответствуют числам 0-25, то код Виженера в общем виде выглядит так:
 #   Шифрование:
-#       Ci=(Pi+Ki) mod 26       -буква -нужная буква -сдвиг?
+#       Ci=(Pi+Ki) mod 26
 #   Расшифровка:
 #       Pi=(Ci-Ki+26) mod 26
+# C = Cipher Text (зашифрованный текст)
+# P = Plain Text (изначальный текст)
+# K = Key (ключ)
 
-from itertools import cycle
+plain_text = input("PLAIN: ")
+key_text = input("KEY: ")
+for key_char in key_text:
+    key_index = alphabet_RU.find(key_char)
+    print(key_index + 1)
 
-user_str = list(input("Слово(а) для шифровки(без пробелов!): ").upper())
-key_word = list(input("Слово(а) ключ(без пробелов!): ").upper())
+for plain_char in plain_text:
+    plain_index = alphabet_RU.find(plain_char)
+    print(plain_index + 1)
 
-if user_str[0] in alphabet_RU:  # Узнаём язык слова
-    alphabet = alphabet_RU
-    print("Язык - русский")
-else:
-    alphabet = alphabet_EU
-    print("Язык - английский")
-# Узнаем индексы букв введенного слова и слова-ключа
-index_key_w = []
-index_user_w = []
-for i in key_word:
-    if i != " ":
-        index_key_w.append(alphabet.index(i) + 1)
-    else:
-        index_key_w.append(0)
-for i in user_str:
-    if i != " ":
-        index_user_w.append(alphabet.index(i) + 1)
-    else:
-        index_user_w.append(0)
-
-
-def encryption(alphabet):  # Функция для шифровки
-    new_index = []
-    for old, new in enumerate(cycle(index_key_w)):  # Высчитываем новый индекс для зашифрованной буквы
-        if old >= len(user_str):
-            break
-        new_index.append(index_user_w[old] + new)
-    return new_index
-
-
-encryption_word = ''.join([alphabet[char - 1] for char in encryption(alphabet)])
-
-
-def decryption(encryption_word, alphabet):  # Функция для расшифровки
-    index_encryption_word = []
-    for i in encryption_word:  # Узнаем индексы букв зашифрованного слова
-        index_encryption_word.append(alphabet.index(i) + 1)
-
-    index_decryption_w = []
-    for old, new in enumerate(cycle(index_key_w)):  # Высчитываем индекс для расшифрованной буквы
-        if old >= len(encryption_word):
-            break
-        index_decryption_w.append(index_encryption_word[old] - new)
-    return index_decryption_w
-
-
-
-
-decryption_word = ''.join([alphabet[char - 1] for char in decryption(encryption_word, alphabet)])
-
-print(f"Зашифрованное слово: {encryption_word}.")
-print(f"Расшифрованное слово: {decryption_word}")
+for char in len(plain_text):
+    print(alphabet_RU[plain_index+key_index])
---
+++ Python/April/210422/1.py
@@ -0,0 +1,35 @@
+# with open("jhgg.txt","r") as files:
+#     line=files.readline()
+#     while line:
+#         print(line,end="")
+#         line=files.readline()
+#         print(type(line))
+# with open("jhgg.txt", "r") as files:
+#     content = files.read()
+#     print(content)
+#     print(type(content))
+# with open("jhgg.txt","r") as files:
+#     contents=files.readlines()
+#     print(contents)
+#     print(type(contents))
+#     str1=contents[0]
+#     str2=contents[1]
+#     print(str1+str2)
+#     print(str1[2:4])
+# b = list()
+# a = input()
+# b.append(a + "\n")
+# while a != "0":
+#     a = input()
+#     b.append(a + "\n")
+# c = b.pop()
+# b.pop(1)
+# print(b)
+# print(c)
+# with open("jhgg.txt", "a") as files:
+#     for i in b:
+#         files.write(i)
+# files.close()
+# with open("jhgg.txt", "r") as files:
+#     for j in files:
+#         print(j, end="")
