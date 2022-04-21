f681409d93318d645b47efbb0dc891d12cc538a7
Student <n  jhvklgyilg>
2022-04-21 13:23:23 +0300

    классная

---
+++ Python/March/claswork/classwork.py
@@ -0,0 +1,24 @@
+with open ("TheFile.txt","r") as the_file:
+    content = the_file.read()
+    print(content)
+    print(type(content))
+
+with open("TheFile.txt", "r") as the_file:
+    contents = the_file.read()
+    print(contents)
+    print(type(contents))
+    string1=contents[0]
+    string2=contents[1]
+    print(strings1, strings2)
+    print(tipe(string1))
+
+    user_input = input("Введите что-нибудь: ")
+    inputs = list()
+    print(inputs)
+
+inputs.pop()#выбрасывает элемент списка
+
+
+
+with open("Thefile.txt", "a") as the_file:
+for massage
