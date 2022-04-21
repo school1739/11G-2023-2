7db92236d1cfd955b370700fb7012b6d956fe15d
451and521rey <andrey.barx@gmail.com>
2022-04-21 13:21:54 +0300

    сделол Андрей

---
+++ Python/april22/april1.py
@@ -0,0 +1,29 @@
+# with open("TheFile.txt", "r") as the_file:
+ #   content = the_file.read()
+ #  print(content)
+ # print(type(content))  # >>> <class'str'>
+    # чтение файла только в одну строку
+#with open("TheFile.txt", "r") as the_file:
+#   contents = the_file.readlines()
+#   print(contents)
+#   print(type(contents))
+#   string1 = contents[0]
+#   string2 = contents[1]
+#   print(string1, string2)
+#   print(type(string1))
+#   print(string1[2:5])
+#   print(contents[1][6:10]#
+
+inputs = list()
+user_input = input("Введите что-нибудь:")
+inputs = list()
+
+while user_input != "0"
+    user_input = input("Введите что-нибудь:")
+    inputs.append(user_input + "\n")
+inputs.pop()
+inputs.pop(1)
+popped = inputs.pop(1)
+
+
+print(inputs)
