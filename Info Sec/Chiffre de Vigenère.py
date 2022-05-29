09288e5989c2b513d6673bf9ec946147fa9f9a8c
Армине Григорян <azaz.ag05@gmail.com>
2022-05-29 12:35:03 +0400

    Info Sec

--- Info Sec/Chiffre de Vigenère.py
+++ Info Sec/Chiffre de Vigenère.py
@@ -1,30 +1,33 @@
-# Напишите программу, шифрующую входящее сообщение с помощью шифра Виженера.
-# Шифр Виженера представляет собой шифр Цезаря с переменной величиной сдвига.
-# Величину сдвига задают ключевым словом.
-# Например слово БАЗА означает последовательность сдвигов исходных букв: 219121912191...
-
 alphabet_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 alphabet_EU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-# HINT (использовать не обязательно):
-# Если буквы A-Z соответствуют числам 0-25, то код Виженера в общем виде выглядит так:
-#   Шифрование:
-#       Ci=(Pi+Ki) mod 26
-#   Расшифровка:
-#       Pi=(Ci-Ki+26) mod 26
-# C = Cipher Text (зашифрованный текст)
-# P = Plain Text (изначальный текст)
-# K = Key (ключ)
 
-plain_text = input("PLAIN: ")
-key_text = input("KEY: ")
-for key_char in key_text:
-    key_index = alphabet_RU.find(key_char)
-    print(key_index + 1)
-
-for plain_char in plain_text:
-    plain_index = alphabet_RU.find(plain_char)
-    print(plain_index + 1)
-
-for char in len(plain_text):
-    print(alphabet_RU[plain_index+key_index])
+key_list=[]
+print("Выбирите язык, указав его порядковый номер.")
+print("1. Русский")
+print("2. Английский")
+number_language = input("Введите номер: ")
+if number_language =="1": # если выбрали русский
+    key_text = str.upper(input("KEY: "))
+    plain_text = str.upper(input("PLAIN: "))
+    for key in key_text:
+        key_list+=key
+    if len(key_text)<len(plain_text):
+        for _ in range((len(plain_text)-len(key_text))//2+1):
+            key_list+=key_list
+    for i in range (len(plain_text)): # смотрим индексы каждой буквы
+        plain_index = alphabet_RU.find(plain_text[i])
+        key_index = alphabet_RU.find(key_list[i])+1
+        print(alphabet_RU[plain_index+key_index]) # складываем индексы и выводим зашифрованное слово
+else: # всё то же самое
+    key_text = input("KEY: ")
+    plain_text = input("PLAIN: ")
+    for key in key_text:
+        key_list+=key
+    if len(key_text)<len(plain_text):
+        for _ in range(len(plain_text)-len(key_text)):
+            key_list+=key_list
+    for i in range (len(plain_text)):
+        plain_index = alphabet_EU.find(plain_text[i])
+        key_index = alphabet_EU.find(key_list[i])+1
+        print(alphabet_EU[plain_index+key_index])
