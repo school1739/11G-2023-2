21e9eb883b15f050f2c0cb575b0d0eb103acbd9e
Армине Григорян <azaz.ag05@gmail.com>
2022-05-30 19:58:16 +0400

    26 ЕГЭ

--- Python/April/250422/Homework/26.py
+++ Python/April/250422/Homework/26.py
@@ -29,3 +29,55 @@
 В данном случае можно купить не более 4 изделий, из них не более 2 изделий B.
 Минимальная цена такой покупки 120 рублей (покупаем изделия 30A, 60B, 20B, 10A).
 Останется 10 рублей. В ответе надо записать числа 2 и 10."""
+
+with open('26.txt', 'r') as file:
+    lines = file.readlines()
+    first_line = lines.pop(0).split()  # отделяем первую строчку
+
+    goods = int(first_line[0])  # кол-во товаров
+    money_1 = int(first_line[1])  #  сумма денег
+
+    lines.sort()  # Сортировка  по возрастанию
+
+    goods_count = 0  # кол-во купленных товаров
+    money_2 = money_1
+
+    # кол-во товара В и Ф
+    B_count = 0
+    A_count = 0
+
+    # пустые списки товаров В и А
+    B_goods = []
+    A_goods = []
+
+    #  списки товаров В и А
+    for line in lines:
+        if line[5] == "B":
+            B_goods.append(line[0:4])
+        elif line[5] == "A":
+            A_goods.append(line[0:4])
+
+    for line in lines:
+        if money_2 >= int(line[0:4]):  # провека условии
+            goods_count += 1
+            money_2 -= int(line[0:4])
+        else:
+            break
+
+    # максимальное количество товаров В
+    for good in B_goods:
+        if money_1 >= int(good):
+            B_count += 1
+            money_1 -= int(good)  #
+        else:
+            break
+
+    while (B_count + A_count < goods_count):
+        if money_1 >= int(A_goods[A_count]):
+            money_1 -= int(A_goods[A_count])
+            A_count += 1
+        else:
+            B_count -= 1
+            money_1 += int(B_goods[B_count])
+
+print(B_count, money_1) #Вывод
