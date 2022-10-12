818e6884d7209978e4c8d417034ce2a4fe0016d8
ladyk <lady.khodina@yandex.ru>
2022-10-12 19:39:21 +0300

    шифр виженера

--- Info Sec/Chiffre de Vigenère.py
+++ Info Sec/Chiffre de Vigenère.py
@@ -5,33 +5,96 @@
 
 alphabet_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 alphabet_EU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-key_list=[] # лист для ключа, если прнадобиться, его можео будет увеличить на смого себя
+key_list = []  # лист для ключа, если прнадобиться, его можео будет увеличить на смого себя
 print("Выбирите язык, указав его порядковый номер.")
 print("1. Русский")
 print("2. Английский")
 nomer_yazyka = input("Введите номер: ")
-if nomer_yazyka =="1": # если выбрали русский
-    key_text = str.upper(input("KEY: "))
-    plain_text = str.upper(input("PLAIN: "))
-    for key in key_text: # преобразуем ключ в список ( не уверена, что это было обязательно делать)
-        key_list+=key
-    if len(key_text)<len(plain_text): # если длина ключа меньше оснвного слова, то увеличиваем его на смого себя
-        for _ in range((len(plain_text)-len(key_text))//2+1): # ( лучше пусть будет больше, чем меньше)
-            key_list+=key_list
-    for i in range (len(plain_text)): # смотрим индексы каждой буквы
-        plain_index = alphabet_RU.find(plain_text[i])
-        key_index = alphabet_RU.find(key_list[i])+1
-        print(alphabet_RU[plain_index+key_index]) # складываем индексы и выводим зашифрованное слово
-else: # всё то же самое, но для английского языка
-    key_text = input("KEY: ")
-    plain_text = input("PLAIN: ")
-    for key in key_text:
-        key_list+=key
-    if len(key_text)<len(plain_text):
-        for _ in range(len(plain_text)-len(key_text)):
-            key_list+=key_list
-    for i in range (len(plain_text)):
-        plain_index = alphabet_EU.find(plain_text[i])
-        key_index = alphabet_EU.find(key_list[i])+1
-        print(alphabet_EU[plain_index+key_index])
-
+answer1 = ""
+answer2 = ""
+print("Выбирите действие, указав его порядковый номер:")
+print("1. Шифровка")
+print("2. Дешифровка")
+print("3. Шифровка в обе стороны")
+variant = input("Ваш выбор: ")
+key_text = str.upper(input("Ключ: "))
+plain_text = str.upper(input("Текст: "))
+for key in key_text:  # преобразуем ключ в список ( не уверена, что это было обязательно делать)
+    key_list += key
+if len(key_text) < len(plain_text):  # если длина ключа меньше оснвного слова, то увеличиваем его на смого себя
+    for _ in range((len(plain_text) - len(key_text)) // 2 + 1):  # ( лучше пусть будет больше, чем меньше)
+        key_list += key_list
+if nomer_yazyka == "1":  # если выбрали русский
+    if variant == "1":
+        for i in range(len(plain_text)):  # смотрим индексы каждой буквы
+            plain_index = alphabet_RU.find(plain_text[i])
+            key_index = alphabet_RU.find(key_list[i]) + 1
+            if plain_text[i] == " ":
+                answer1 += " "
+            else:
+                answer1 += alphabet_RU[plain_index + key_index]  # складываем индыксы
+        print("Рузультат:", answer1)  # выводим зашифрованное слово
+    # Далее всё аналогично первому примеру
+    if variant == "2":
+        for i in range(len(plain_text)):
+            plain_index = alphabet_RU.find(plain_text[i])
+            key_index = alphabet_RU.find(key_list[i]) + 1
+            if plain_text[i] == " ":
+                answer1 += " "
+            else:
+                answer1 += alphabet_RU[plain_index - key_index]
+        print("Рузультат:", answer1)
+    if variant == "3":
+        for i in range(len(plain_text)):
+            plain_index = alphabet_RU.find(plain_text[i])
+            key_index = alphabet_RU.find(key_list[i]) + 1
+            if plain_text[i] == " ":
+                answer1 += " "
+            else:
+                answer1 += alphabet_RU[plain_index + key_index]
+        print("Рузультат:", answer1)
+        for i in range(len(plain_text)):
+            plain_index = alphabet_RU.find(plain_text[i])
+            key_index = alphabet_RU.find(key_list[i]) + 1
+            if plain_text[i] == " ":
+                answer2 += " "
+            else:
+                answer2 += alphabet_RU[plain_index - key_index]
+        print("Рузультат:", answer2)
+if nomer_yazyka == "2":  # всё то же самое, но для английского языка
+    if variant == "1":
+        for i in range(len(plain_text)):  # смотрим индексы каждой буквы
+            plain_index = alphabet_EU.find(plain_text[i])
+            key_index = alphabet_EU.find(key_list[i]) + 1
+            if plain_text[i] == " ":
+                answer1 += " "
+            else:
+                answer1 += alphabet_EU[plain_index + key_index]  # складываем индыксы
+        print("Рузультат:", answer1)  # выводим зашифрованное слово
+    # Далее всё аналогично первому примеру
+    if variant == "2":
+        for i in range(len(plain_text)):
+            plain_index = alphabet_EU.find(plain_text[i])
+            key_index = alphabet_EU.find(key_list[i]) + 1
+            if plain_text[i] == " ":
+                answer1 += " "
+            else:
+                answer1 += alphabet_EU[plain_index - key_index]
+        print("Рузультат:", answer1)
+    if variant == "3":
+        for i in range(len(plain_text)):
+            plain_index = alphabet_EU.find(plain_text[i])
+            key_index = alphabet_EU.find(key_list[i]) + 1
+            if plain_text[i] == " ":
+                answer1 += " "
+            else:
+                answer1 += alphabet_EU[plain_index + key_index]
+        print("Рузультат:", answer1)
+        for i in range(len(plain_text)):
+            plain_index = alphabet_EU.find(plain_text[i])
+            key_index = alphabet_EU.find(key_list[i]) + 1
+            if plain_text[i] == " ":
+                answer2 += " "
+            else:
+                answer2 += alphabet_EU[plain_index - key_index]
+        print("Рузультат:", answer2)
--- Info Sec/October/1210222/classwork.py
+++ Info Sec/October/1210222/classwork.py
@@ -5,58 +5,59 @@ print("1. Русский")
 print("2. Английский")
 nomer_yazyka = input("Введите номер: ")
 print("Выбирите действие, указав его порядковый номер:")
-print("1.Шифровка")
-print("2.Дешифровка")
-print("3.Все возможные шифроки")
+print("1. Шифровка")
+print("2. Дешифровка")
+print("3. Все возможные шифроки")
 variant = input("Ваш выбор: ")
-if variant=="1" or variant=="2":
-    key= int(input("Введите ключ: "))
-text=str.upper(input("Введите текст: "))
-answer=''
-if nomer_yazyka=="1":
-    if variant=="1":
+if variant == "1" or variant == "2":
+    key = int(input("Введите ключ: "))
+text = str.upper(input("Введите текст: "))
+answer = ''
+if nomer_yazyka == "1": # если выбрали русский язык
+    if variant == "1":
         for l in text:
-            if l==" ":
-                answer+=" "
+            if l == " ":
+                answer += " "
             else:
-                answer += alphabet_RU[(alphabet_RU.index(l) + key) % len(alphabet_RU)]
-        print("Рузультат:", answer)
-    if variant=="2":
+                answer += alphabet_RU[(alphabet_RU.index(l) + key) % len(alphabet_RU)] # получаем ответ
+        print("Рузультат:", answer) # выводим ответ
+# далее всё аналогично первому примеру
+    if variant == "2":
         for l in text:
-            if l==" ":
-                answer+=" "
+            if l == " ":
+                answer += " "
             else:
-                answer+=alphabet_RU[(alphabet_RU.index(l)-key)%len(alphabet_RU)]
+                answer += alphabet_RU[(alphabet_RU.index(l) - key) % len(alphabet_RU)]
         print("Рузультат:", answer)
-    if variant=="3":
-        for i in range(1,33):
-            key=i
-            answer=""
+    if variant == "3":
+        for i in range(1, 33):
+            key = i
+            answer = ""
             for l in text:
                 if l == " ":
                     answer += " "
                 else:
                     answer += alphabet_RU[(alphabet_RU.index(l) + key) % len(alphabet_RU)]
             print("Рузультат:", answer)
-if nomer_yazyka=="2":
-    if variant=="1":
+if nomer_yazyka == "2":
+    if variant == "1":
         for l in text:
-            if l==" ":
-                answer+=" "
+            if l == " ":
+                answer += " "
             else:
                 answer += alphabet_EU[(alphabet_EU.index(l) + key) % len(alphabet_EU)]
         print("Рузультат:", answer)
-    if variant=="2":
+    if variant == "2":
         for l in text:
-            if l==" ":
-                answer+=" "
+            if l == " ":
+                answer += " "
             else:
-                answer+=alphabet_EU[(alphabet_EU.index(l)-key)%len(alphabet_EU)]
+                answer += alphabet_EU[(alphabet_EU.index(l) - key) % len(alphabet_EU)]
         print("Рузультат:", answer)
-    if variant=="3":
-        for i in range(1,26):
-            key=i
-            answer=""
+    if variant == "3":
+        for i in range(1, 26):
+            key = i
+            answer = ""
             for l in text:
                 if l == " ":
                     answer += " "
