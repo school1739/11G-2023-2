0ec2642f2781f44b54d6a5a0eb509769df77b3c5
Anikanov Fedor <anikanov.fedor01@gmail.com>
2022-10-25 19:45:40 +0300

    шифр цезаря

--- Python/цезарь.py
+++ Python/цезарь.py
@@ -1,95 +1,52 @@
-lphabet_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-alphabet_EU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-key_list = []  # лист для ключа, если понадобиться, его можно будет увеличить на самого себя
-print("Выбирите язык, указав его порядковый номер.")
-print("1. Русский")
-print("2. Английский")
-nomer_yazyka = input("Введите номер: ")
-answer1 = ""
-answer2 = ""
-print("Выбирите действие, указав его порядковый номер:")
-print("1. Шифровка")
-print("2. Дешифровка")
-print("3. Шифровка в обе стороны")
-variant = input("Ваш выбор: ")
-key_text = str.upper(input("Ключ: "))
-plain_text = str.upper(input("Текст: "))
-for key in key_text:  # преобразуем ключ в список
-    key_list += key
-if len(key_text) < len(plain_text):  # если длина ключа меньше основного слова, то увеличиваем его на самого себя
-    for _ in range((len(plain_text) - len(key_text)) // 2 + 1):
-        key_list += key_list
-if nomer_yazyka == "1":  # если выбрали русский язык
-    if variant == "1":
-        for i in range(len(plain_text)):  # смотрим индексы каждой буквы
-            plain_index = alphabet_RU.find(plain_text[i])
-            key_index = alphabet_RU.find(key_list[i]) + 1
-            if plain_text[i] == " ":
-                answer1 += " "
+choice = int(input("1 - шифрование, 2 - дешифровка, 3 - перебрать брутом --> "))
+l = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def chifr(sdvig): #Функция для шифровки
+    global l
+    stroka_for_shifr = input("Вы выбрали режим шифрование, введите строку для шифрования(only english, а то русскую лень делать, та и по заданию уточнений не было:)) --> ").upper()
+    itog_shifr = ""
+    for i in stroka_for_shifr:
+        ind = l.find(i)
+        new_ind = ind + sdvig
+        if i in l: itog_shifr += l[new_ind]
+
+        else: itog_shifr += i
+    return itog_shifr
+
+def deshifr(isv_sdvig): #Функция для дешифровки
+    global l
+    stroka_for_deshifr = input("Вы выбрали режим дешифрование, введите строку  для дешифрования(only english, а то русскую лень делать, та и по заданию уточнений не было:)) --> ").upper()
+    itog_deshifr = ""
+    for i in stroka_for_deshifr:
+        ind = l.find(i)
+        new_ind = ind - isv_sdvig
+        if i in l: itog_deshifr += l[new_ind]
+        else:itog_deshifr += i
+    return itog_deshifr
+
+
+
+def brut(): #Функция для брута
+    global l
+    stroka_for_shifr = input("Вы выбрали режим брут, введите строку для дешифрования(only english, а то русскую лень делать, та и по заданию уточнений не было:))--> ").upper()
+    for sdvig in range(1, 26):
+        itog_shifr = ""
+        for i in stroka_for_shifr:
+            mesto = l.find(i)
+            new_mesto = mesto + sdvig
+            if i in l:
+                itog_shifr += l[new_mesto]
             else:
-                answer1 += alphabet_RU[plain_index + key_index]  # складываем индыксы
-        print("Рузультат:", answer1)  # выводим зашифрованное слово
-    # Далее по аналогии  первому примеру
-    if variant == "2":
-        for i in range(len(plain_text)):
-            plain_index = alphabet_RU.find(plain_text[i])
-            key_index = alphabet_RU.find(key_list[i]) + 1
-            if plain_text[i] == " ":
-                answer1 += " "
-            else:
-                answer1 += alphabet_RU[plain_index - key_index]
-        print("Рузультат:", answer1)
-    if variant == "3":
-        for i in range(len(plain_text)):
-            plain_index = alphabet_RU.find(plain_text[i])
-            key_index = alphabet_RU.find(key_list[i]) + 1
-            if plain_text[i] == " ":
-                answer1 += " "
-            else:
-                answer1 += alphabet_RU[plain_index + key_index]
-        print("Рузультат:", answer1)
-        for i in range(len(plain_text)):
-            plain_index = alphabet_RU.find(plain_text[i])
-            key_index = alphabet_RU.find(key_list[i]) + 1
-            if plain_text[i] == " ":
-                answer2 += " "
-            else:
-                answer2 += alphabet_RU[plain_index - key_index]
-        print("Рузультат:", answer2)
-if nomer_yazyka == "2":  # всё то же самое, но для английского языка
-    if variant == "1":
-        for i in range(len(plain_text)):  # смотрим индексы каждой буквы
-            plain_index = alphabet_EU.find(plain_text[i])
-            key_index = alphabet_EU.find(key_list[i]) + 1
-            if plain_text[i] == " ":
-                answer1 += " "
-            else:
-                answer1 += alphabet_EU[plain_index + key_index]  # складываем индыксы
-        print("Рузультат:", answer1)  # выводим зашифрованное слово
-    # Далее всё по аналогии  первому примеру
-    if variant == "2":
-        for i in range(len(plain_text)):
-            plain_index = alphabet_EU.find(plain_text[i])
-            key_index = alphabet_EU.find(key_list[i]) + 1
-            if plain_text[i] == " ":
-                answer1 += " "
-            else:
-                answer1 += alphabet_EU[plain_index - key_index]
-        print("Рузультат:", answer1)
-    if variant == "3":
-        for i in range(len(plain_text)):
-            plain_index = alphabet_EU.find(plain_text[i])
-            key_index = alphabet_EU.find(key_list[i]) + 1
-            if plain_text[i] == " ":
-                answer1 += " "
-            else:
-                answer1 += alphabet_EU[plain_index + key_index]
-        print("Рузультат:", answer1)
-        for i in range(len(plain_text)):
-            plain_index = alphabet_EU.find(plain_text[i])
-            key_index = alphabet_EU.find(key_list[i]) + 1
-            if plain_text[i] == " ":
-                answer2 += " "
-            else:
-                answer2 += alphabet_EU[plain_index - key_index]
-        print("Рузультат:", answer2)
+                itog_shifr += i
+        print("При sdvig == " + str(sdvig) + " шифровщик выдает - " + str(itog_shifr))
+
+if choice == 1:
+    sdvig = int(input("Введите сдвиг --> "))
+    print(chifr(sdvig))
+
+if choice == 2:
+    isv_sdvig = int(input("Введите сдвиг, который использовался при шифровании строки --> "))
+    print(deshifr(isv_sdvig))
+
+if choice == 3:
+    brut()
